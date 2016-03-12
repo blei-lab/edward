4c8be1dd8788b5dc8526ca212ed6d181a858d7c3
mrudolph <mrr2163@columbia.edu>
2016-03-11 19:00:28 -0500

    need to fix log_prob_z_i for MFPointMass in blackbox/variationals.py

--- blackbox/models.py
+++ blackbox/models.py
@@ -2,8 +2,6 @@ from __future__ import print_function
 import numpy as np
 import tensorflow as tf
 
-from variationals import MFPointMass
-
 try:
     import pystan
     from collections import OrderedDict
