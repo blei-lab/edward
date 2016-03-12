5248addce2f959bf01a7835076e19372127ac50b
Adji Bousso Dieng <diengadji@gmail.com>
2016-03-11 22:26:11 -0500

    updated mix gaussian example

--- examples/mixture_gaussian.py
+++ examples/mixture_gaussian.py
@@ -55,11 +55,11 @@ model{
 """
 bb.set_seed(42)
 model = bb.StanModel(model_code=model_code)
-variational = bb.MFMixGaussian(1)
+K = 2
+variational = bb.MFMixGaussian(1, K)
 x = np.loadtxt('../data/mix_mock_data.txt', dtype='float32', delimiter=',')
 N = len(X)
 D = 2
-K = 2
 data = bb.Data(dict(N=N, K=K, D=D , x=x))
 
 inference = bb.MFVI(model, variational, data)
