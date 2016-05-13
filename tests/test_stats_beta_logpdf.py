2926b0f086a39068c8932da2a08342e8043f5bd4
Dustin Tran <dustinviettran@gmail.com>
2016-05-13 18:03:03 -0400

    clean up test_stats_beta_logpdf.py

--- tests/test_stats_beta_logpdf.py
+++ tests/test_stats_beta_logpdf.py
@@ -13,7 +13,7 @@ def _assert_eq(val_ed, val_true):
         # only an approximation
         assert np.allclose(val_ed.eval(), val_true, atol=1e-4)
 
-def _test_logpdf_scalar(x, a=0.5, b=0.5):
+def _test_logpdf(x, a=0.5, b=0.5):
     xtf = tf.constant(x)
     val_true = stats.beta.logpdf(x, a, b)
     _assert_eq(beta.logpdf(xtf, tf.constant(a), tf.constant(b)), val_true)
@@ -22,23 +22,17 @@ def _test_logpdf_scalar(x, a=0.5, b=0.5):
     _assert_eq(beta.logpdf(xtf, tf.constant([a]), tf.constant([b])), val_true)
 
 def test_logpdf_scalar():
-    _test_logpdf_scalar(0.3)
-    _test_logpdf_scalar(0.7)
+    _test_logpdf(0.3)
+    _test_logpdf(0.7)
 
-    _test_logpdf_scalar(0.3, a=1.0, b=1.0)
-    _test_logpdf_scalar(0.7, a=1.0, b=1.0)
+    _test_logpdf(0.3, a=1.0, b=1.0)
+    _test_logpdf(0.7, a=1.0, b=1.0)
 
-    _test_logpdf_scalar(0.3, a=0.5, b=5.0)
-    _test_logpdf_scalar(0.7, a=0.5, b=5.0)
+    _test_logpdf(0.3, a=0.5, b=5.0)
+    _test_logpdf(0.7, a=0.5, b=5.0)
 
-    _test_logpdf_scalar(0.3, a=5.0, b=0.5)
-    _test_logpdf_scalar(0.7, a=5.0, b=0.5)
+    _test_logpdf(0.3, a=5.0, b=0.5)
+    _test_logpdf(0.7, a=5.0, b=0.5)
 
 def test_logpdf_1d():
-    x = [0.5, 0.3, 0.8, 0.1]
-    xtf = tf.constant(x)
-    val_true = stats.beta.logpdf(x, 0.5, 0.5)
-    _assert_eq(beta.logpdf(xtf, tf.constant(0.5), tf.constant(0.5)), val_true)
-    _assert_eq(beta.logpdf(xtf, tf.constant([0.5]), tf.constant(0.5)), val_true)
-    _assert_eq(beta.logpdf(xtf, tf.constant(0.5), tf.constant([0.5])), val_true)
-    _assert_eq(beta.logpdf(xtf, tf.constant([0.5]), tf.constant([0.5])), val_true)
+    _test_logpdf([0.5, 0.3, 0.8, 0.1], a=0.5, b=0.5)
