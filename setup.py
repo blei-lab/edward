c5757f67a34d53041295bde1a37c5450f89c748e
Dustin Tran <dustinviettran@gmail.com>
2016-05-14 10:45:09 -0400

    version 1.0.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='edward',
-    version='1.0.3',
+    version='1.0.4',
     description='A library for probabilistic modeling, inference, and criticism',
     author='Dustin Tran',
     author_email="dustin@cs.columbia.edu",
