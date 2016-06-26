0b584445d80cac10b7aefc1ab837335e470b50fe
Dustin Tran <dustinviettran@gmail.com>
2016-06-25 22:18:58 -0400

    let test_hessian pass for tf==0.6

--- tests/test_hessian.py
+++ tests/test_hessian.py
@@ -14,8 +14,8 @@ def _test(y, xs, val_true):
         assert np.all(val_est == val_true)
 
 def test_scalar():
-    x1 = tf.Variable(tf.random_normal([1]), dtype=tf.float32)
-    x2 = tf.Variable(tf.random_normal([1]), dtype=tf.float32)
+    x1 = tf.Variable(tf.random_normal([1], dtype=tf.float32))
+    x2 = tf.Variable(tf.random_normal([1], dtype=tf.float32))
     y = tf.pow(x1, tf.constant(2.0)) + tf.constant(2.0) * x1 * x2 + \
         tf.constant(3.0) * tf.pow(x2, tf.constant(2.0)) + \
         tf.constant(4.0) * x1 + tf.constant(5.0) * x2 + tf.constant(6.0)
@@ -23,20 +23,20 @@ def test_scalar():
     _test(y, [x2], val_true=np.array([[6.0]]))
 
 def test_1d():
-    x1 = tf.Variable(tf.random_normal([1]), dtype=tf.float32)
-    x2 = tf.Variable(tf.random_normal([1]), dtype=tf.float32)
+    x1 = tf.Variable(tf.random_normal([1], dtype=tf.float32))
+    x2 = tf.Variable(tf.random_normal([1], dtype=tf.float32))
     y = tf.pow(x1, tf.constant(2.0)) + tf.constant(2.0) * x1 * x2 + \
         tf.constant(3.0) * tf.pow(x2, tf.constant(2.0)) + \
         tf.constant(4.0) * x1 + tf.constant(5.0) * x2 + tf.constant(6.0)
     _test(y, [x1, x2], val_true=np.array([[2.0, 2.0], [2.0, 6.0]]))
-    x3 = tf.Variable(tf.random_normal([3]), dtype=tf.float32)
+    x3 = tf.Variable(tf.random_normal([3], dtype=tf.float32))
     y = tf.pow(x2, tf.constant(2.0)) + tf.reduce_sum(x3)
     _test(y, [x3], val_true=np.zeros([3, 3]))
     _test(y, [x2, x3], val_true=np.diag([2.0, 0.0, 0.0, 0.0]))
 
 def test_2d():
-    x1 = tf.Variable(tf.random_normal([3, 2]), dtype=tf.float32)
-    x2 = tf.Variable(tf.random_normal([2]), dtype=tf.float32)
+    x1 = tf.Variable(tf.random_normal([3, 2], dtype=tf.float32))
+    x2 = tf.Variable(tf.random_normal([2], dtype=tf.float32))
     y = tf.reduce_sum(tf.pow(x1, tf.constant(2.0))) + tf.reduce_sum(x2)
     _test(y, [x1], val_true=np.diag([2.0]*6))
     _test(y, [x1, x2], val_true=np.diag([2.0]*6+[0.0]*2))
