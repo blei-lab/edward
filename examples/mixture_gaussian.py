8ce56fe0a344d5c2cdf1a4c3c614fca91d11631f
Dustin Tran <dustinviettran@gmail.com>
2016-06-28 01:56:32 -0400

    get mixture_gaussian.py working with n_minibatch > 1

--- examples/mixture_gaussian.py
+++ examples/mixture_gaussian.py
@@ -62,8 +62,6 @@ class MixtureGaussian:
         N = get_dims(xs)[0]
         pi, mus, sigmas = zs
         log_prior = dirichlet.logpdf(pi, self.alpha)
-        #log_prior += tf.reduce_sum(norm.logpdf(mus, 0, np.sqrt(self.c)), 1)
-        #log_prior += tf.reduce_sum(invgamma.logpdf(sigmas, self.a, self.b), 1)
         log_prior += tf.reduce_sum(norm.logpdf(mus, 0, np.sqrt(self.c)))
         log_prior += tf.reduce_sum(invgamma.logpdf(sigmas, self.a, self.b))
 
@@ -79,16 +77,27 @@ class MixtureGaussian:
 
         return tf.squeeze(log_prior + tf.pack(log_lik))
 
-        #for s in range(n_minibatch):
-        #    log_lik_z = N*tf.reduce_sum(tf.log(pi), 1)
-        #    for k in range(self.K):
-        #        log_lik_z += tf.reduce_sum(multivariate_normal.logpdf(xs,
-        #            mus[s, (k*self.D):((k+1)*self.D)],
-        #            sigmas[s, (k*self.D):((k+1)*self.D)]))
+    def log_prob(self, xs, zs):
+        """Returns a vector [log p(xs, zs[1,:]), ..., log p(xs, zs[S,:])]."""
+        N = get_dims(xs)[0]
+        pi, mus, sigmas = zs
+        log_prior = dirichlet.logpdf(pi, self.alpha)
+        log_prior += tf.reduce_sum(norm.logpdf(mus, 0, np.sqrt(self.c)), 1)
+        log_prior += tf.reduce_sum(invgamma.logpdf(sigmas, self.a, self.b), 1)
 
-        #    log_lik += [log_lik_z]
+        # Loop over each mini-batch zs[b,:]
+        log_lik = []
+        n_minibatch = get_dims(zs[0])[0]
+        for s in range(n_minibatch):
+            log_lik_z = N*tf.reduce_sum(tf.log(pi), 1)
+            for k in range(self.K):
+                log_lik_z += tf.reduce_sum(multivariate_normal.logpdf(xs,
+                    mus[s, (k*self.D):((k+1)*self.D)],
+                    sigmas[s, (k*self.D):((k+1)*self.D)]))
 
-        #return log_prior + tf.pack(log_lik)
+            log_lik += [log_lik_z]
+
+        return log_prior + tf.pack(log_lik)
 
 ed.set_seed(42)
 x = np.loadtxt('data/mixture_data.txt', dtype='float32', delimiter=',')
@@ -96,13 +105,9 @@ data = ed.Data(tf.constant(x, dtype=tf.float32))
 
 model = MixtureGaussian(K=2, D=2)
 variational = Variational()
-#variational.add(Dirichlet([1, model.K])) # TODO should this be allowed?
 variational.add(Dirichlet(model.K))
 variational.add(Normal(model.K*model.D))
 variational.add(InvGamma(model.K*model.D))
 
 inference = ed.MFVI(model, variational, data)
-# TODO
-inference.run(n_iter=500, n_minibatch=1, n_data=5)
-# TODO
-#inference.run(n_iter=500, n_minibatch=5, n_data=5)
+inference.run(n_iter=500, n_minibatch=5, n_data=5)
