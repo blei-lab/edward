2ace0db91014b7e0ef9c913bbc3f7247a740ed54
Adji Bousso Dieng <diengadji@gmail.com>
2016-06-29 10:01:10 -0700

    created directory for creating Edward inhouse datasets

---
+++ edward/datasets/benchmark.py
@@ -0,0 +1,13 @@
+"""
+this should contain methods such as:
+ load_mnist()
+ load_crabs()
+ load_ionosphere()
+ load_pima()
+ load_heart()
+ load_ptb()
+ load_iris()
+ etc...
+"""
+
+import tensorflow as tf
