387925cced84e2423b933771604dbda8b275fcc8
Adji Bousso Dieng <diengadji@gmail.com>
2016-06-29 10:01:10 -0700

    created directory for creating Edward inhouse datasets

---
+++ edward/datasets/simulated.py
@@ -0,0 +1,6 @@
+"""
+this should contain simulated data from a well
+defined generative process...
+"""
+
+import tensorflow as tf
---
+++ edward/datasets/toy.py
@@ -0,0 +1,8 @@
+"""
+this should contain methods such as:
+  load_toy_classification_data()
+  load_toy_regression_data()
+  etc...
+"""
+
+import tensorflow as tf
