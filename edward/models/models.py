09bf8355f212a88ba3bf835797f195223bde44e8
Dustin Tran <dustinviettran@gmail.com>
2016-07-03 18:11:50 -0700

    prevent class copy of layers object

--- edward/models/models.py
+++ edward/models/models.py
@@ -151,10 +151,10 @@ class StanModel:
 
 class Variational:
     """A container for collecting distribution objects."""
-    def __init__(self, layers=[]):
+    def __init__(self, layers=None):
         get_session()
-        self.layers = layers
-        if layers == []:
+        if layers is None:
+            self.layers = []
             self.shape = []
             self.num_vars = 0
             self.num_params = 0
@@ -164,6 +164,7 @@ class Variational:
             self.sample_tensor = []
             self.is_multivariate = []
         else:
+            self.layers = layers
             self.shape = [layer.shape for layer in self.layers]
             self.num_vars = sum([layer.num_vars for layer in self.layers])
             self.num_params = sum([layer.num_params for layer in self.layers])
