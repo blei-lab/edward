378ad65178aa99999510e0d357f3ac9f0dc91e57
Dustin Tran <dustinviettran@gmail.com>
2016-07-07 17:02:13 -0700

    convert unit tests to follow tensorflow style guidelines

--- tests/test-stats/test_lognorm_logpdf.py
+++ tests/test-stats/test_lognorm_logpdf.py
@@ -1,4 +1,7 @@
+from __future__ import absolute_import
+from __future__ import division
 from __future__ import print_function
+
 import numpy as np
 import tensorflow as tf
 
@@ -7,9 +10,11 @@ from scipy import stats
 
 sess = tf.Session()
 
+
 def _assert_eq(val_ed, val_true):
     with sess.as_default():
         assert np.allclose(val_ed.eval(), val_true)
+
 
 def _test(x, s):
     xtf = tf.constant(x)
@@ -18,12 +23,15 @@ def _test(x, s):
     _assert_eq(lognorm.logpdf(xtf, tf.constant(s)), val_true)
     _assert_eq(lognorm.logpdf(xtf, tf.constant([s])), val_true)
 
+
 def test_scalar():
     _test(2.0, s=1)
     _test(0.623, s=1)
 
+
 def test_1d():
     _test([2.0, 1.0, 0.58, 2.3], s=1)
+
 
 def test_2d():
     _test(np.array([[2.0, 1.0, 0.58, 2.3],[2.1, 1.3, 1.58, 0.3]]),
--- tests/test-stats/test_norm_logpdf.py
+++ tests/test-stats/test_norm_logpdf.py
@@ -1,4 +1,7 @@
+from __future__ import absolute_import
+from __future__ import division
 from __future__ import print_function
+
 import numpy as np
 import tensorflow as tf
 
@@ -7,9 +10,11 @@ from scipy import stats
 
 sess = tf.Session()
 
+
 def _assert_eq(val_ed, val_true):
     with sess.as_default():
         assert np.allclose(val_ed.eval(), val_true)
+
 
 def _test(x, loc=0, scale=1):
     xtf = tf.constant(x)
@@ -20,12 +25,15 @@ def _test(x, loc=0, scale=1):
     _assert_eq(norm.logpdf(xtf, tf.constant(loc), tf.constant([scale])), val_true)
     _assert_eq(norm.logpdf(xtf, tf.constant([loc]), tf.constant([scale])), val_true)
 
+
 def test_scalar():
     _test(0.0)
     _test(0.623)
 
+
 def test_1d():
     _test([0.0, 1.0, 0.58, 2.3])
+
 
 def test_2d():
     _test(np.array([[0.0, 1.0, 0.58, 2.3], [0.1, 1.5, 4.18, 0.3]]))
