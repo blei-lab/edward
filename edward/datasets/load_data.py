3a3ee834f0b503904dc6eea35ac45abb1cf3a58f
Maja <marirudolph@gmail.com>
2016-07-16 14:09:56 -0400

    gp classification and latent space model

--- edward/datasets/load_data.py
+++ edward/datasets/load_data.py
@@ -8,3 +8,17 @@ def load_mixture_data():
     module_path = dirname(__file__)
     x = np.loadtxt(join(module_path, 'data', 'mixture_data.txt'), dtype='float32', delimiter=',')
     return {'x': x}
+
+def load_crabs_data(N=None):
+    module_path = dirname(__file__)
+    df = np.loadtxt(join(module_path, 'data', 'crabs_train.txt'), dtype='float32', delimiter=',')
+    if type(N)!= int or N>len(df):
+        N = len(df)
+    return {'x': df[:N, 1:], 'y': df[:N, 0]}
+
+def load_celegans_brain():
+    module_path = dirname(__file__)
+    x = np.load(join(module_path, 'data', 'celegans_brain.npy'))
+    return {'x': x}
+
+
