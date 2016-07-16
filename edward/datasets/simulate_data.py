40194ce5430c2d986dfd56a638b1fad03c6c2f27
Maja <marirudolph@gmail.com>
2016-07-16 13:50:06 -0400

    logistic regression example

--- edward/datasets/simulate_data.py
+++ edward/datasets/simulate_data.py
@@ -4,11 +4,14 @@ import tensorflow as tf
 from edward.stats import norm
 from sklearn.cross_validation import train_test_split
 
-def simulate_regression_data(N=40, noise_std=0.1, coeff=None, n_dims = 1):
+def simulate_regression_data(N=40, noise_std=0.1, coeff=None, n_dims = 1, transform=None):
     if coeff == None and n_dims ==1:
         x  = np.concatenate([np.linspace(0, 2, num=N/2),
                              np.linspace(6, 8, num=N/2)])
-        y = 0.075*x + norm.rvs(0, noise_std, size=N)
+        if transform == 'cosine':
+            y = np.cos(x) + norm.rvs(0, noise_std, size=N)
+        else:
+            y = 0.075*x + norm.rvs(0, noise_std, size=N)
         x = (x - 4.0) / 4.0
         x = x.reshape((N, 1))
     elif coeff == None:
@@ -19,6 +22,16 @@ def simulate_regression_data(N=40, noise_std=0.1, coeff=None, n_dims = 1):
         x = np.random.randn(N, n_dims).astype(np.float32)
         y = np.dot(x, coeff) + norm.rvs(0, noise_std, size=N)
     return {'x': x, 'y': y}
+
+def simulate_binary_classification_data(N=40, D=1, noise_std=0.1):
+    x  = np.linspace(-3, 3, num=N)
+    y = np.tanh(x) + norm.rvs(0, noise_std, size=N)
+    y[y < 0.5] = 0
+    y[y >= 0.5] = 1
+    x = (x - 4.0) / 4.0
+    x = x.reshape((N, D))
+    return {'x': x, 'y': y}
+
 
 def simulate_mdn_data(N=6000,test_split=True):
     x = tf.placeholder(tf.float32, shape=(None, 1))
