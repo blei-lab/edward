dec179ef29ed2a6f3441f76966c1f9f0d9dbfd9b
Dustin Tran <dustinviettran@gmail.com>
2016-07-19 23:48:20 -0700

    add beta bernoulli native

---
+++ examples/beta_bernoulli_native.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+"""
+A simple coin flipping example. The model is written in TensorFlow.
+Inspired by Stan's toy example.
+
+Probability model
+    Prior: Beta
+    Likelihood: Bernoulli
+Variational model
+    Likelihood: Mean-field Beta
+"""
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import edward as ed
+import numpy as np
+import tensorflow as tf
+
+from edward.models import Model, Beta
+from edward.stats import bernoulli, beta
+
+
+class BetaBernoulli:
+    """p(x, z) = Bernoulli(x | z) * Beta(z | 1, 1)"""
+    def log_prob(self, var_dict):
+        xs, zs = var_dict['x'], var_dict['z']
+        log_prior = beta.logpdf(zs, a=1.0, b=1.0)
+        log_lik = tf.pack([tf.reduce_sum(bernoulli.logpmf(xs, z))
+                           for z in tf.unpack(zs)])
+        return log_lik + log_prior
+
+
+ed.set_seed(42)
+model = BetaBernoulli()
+
+qz = Beta()
+
+data = {'x': np.array([0, 1, 0, 0, 0, 0, 0, 0, 0, 1])}
+
+inference = ed.MFVI({'z': qz}, data, model)
+inference.run(n_iter=10000)
