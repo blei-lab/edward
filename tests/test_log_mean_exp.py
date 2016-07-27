4c2d518a3b9299e8110a45c385a538adafe2a071
Alp Kucukelbir <akucukelbir@gmail.com>
2016-07-27 09:43:17 -0400

    updated existing tests

--- tests/test_log_mean_exp.py
+++ tests/test_log_mean_exp.py
@@ -2,23 +2,33 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-import numpy as np
 import tensorflow as tf
+import numpy as np
 
 from edward.util import log_mean_exp
 
-sess = tf.InteractiveSession()
+class test_log_mean_exp(tf.test.TestCase):
 
+    def test_log_mean_exp_1d(self):
+        with self.test_session():
+            x = tf.constant([-1.0, -2.0, -3.0, -4.0])
+            self.assertAllClose(log_mean_exp(x).eval(), 
+                                -1.9461046625586951)
+            
+    def test_log_mean_exp_2d(self):
+        with self.test_session():
+            x = tf.constant([[-1.0], [-2.0], [-3.0], [-4.0]])
+            self.assertAllClose(log_mean_exp(x).eval(), 
+                                -1.9461046625586951)             
 
-def test_1d():
-    x = tf.constant([-1.0, -2.0, -3.0, -4.0])
-    val_ed = log_mean_exp(x)
-    val_true = -1.9461046625586951
-    assert np.allclose(val_ed.eval(), val_true)
+    def test_all_finite_raises(self):
+        with self.test_session():
+            x = np.inf * tf.constant([-1.0, -2.0, -3.0, -4.0])
+            with self.assertRaisesOpError('Inf'):
+                log_mean_exp(x).eval()
+            x = tf.constant([-1.0, np.nan, -3.0, -4.0])
+            with self.assertRaisesOpError('NaN'):
+                log_mean_exp(x).eval()                
 
-
-def test_2d():
-    x = tf.constant([[-1.0], [-2.0], [-3.0], [-4.0]])
-    val_ed = log_mean_exp(x)
-    val_true = -1.9461046625586951
-    assert np.allclose(val_ed.eval(), val_true)
+if __name__ == '__main__':
+    tf.test.main()
--- tests/test_log_sum_exp.py
+++ tests/test_log_sum_exp.py
@@ -2,23 +2,33 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-import numpy as np
 import tensorflow as tf
+import numpy as np
 
 from edward.util import log_sum_exp
 
-sess = tf.InteractiveSession()
+class test_log_sum_exp(tf.test.TestCase):
 
+    def test_log_sum_exp_1d(self):
+        with self.test_session():
+            x = tf.constant([-1.0, -2.0, -3.0, -4.0])
+            self.assertAllClose(log_sum_exp(x).eval(), 
+                                -0.5598103014388045)
+            
+    def test_log_sum_exp_2d(self):
+        with self.test_session():
+            x = tf.constant([[-1.0], [-2.0], [-3.0], [-4.0]])
+            self.assertAllClose(log_sum_exp(x).eval(), 
+                                -0.5598103014388045)             
 
-def test_1d():
-    x = tf.constant([-1.0, -2.0, -3.0, -4.0])
-    val_ed = log_sum_exp(x)
-    val_true = -0.5598103014388045
-    assert np.allclose(val_ed.eval(), val_true)
+    def test_all_finite_raises(self):
+        with self.test_session():
+            x = np.inf * tf.constant([-1.0, -2.0, -3.0, -4.0])
+            with self.assertRaisesOpError('Inf'):
+                log_sum_exp(x).eval()
+            x = tf.constant([-1.0, np.nan, -3.0, -4.0])
+            with self.assertRaisesOpError('NaN'):
+                log_sum_exp(x).eval()                
 
-
-def test_2d():
-    x = tf.constant([[-1.0], [-2.0], [-3.0], [-4.0]])
-    val_ed = log_sum_exp(x)
-    val_true = -0.5598103014388045
-    assert np.allclose(val_ed.eval(), val_true)
+if __name__ == '__main__':
+    tf.test.main()
