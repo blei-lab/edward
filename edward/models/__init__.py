cff83fdd500ea819923cb071c11e3783055a7cca
Dustin Tran <dustinviettran@gmail.com>
2016-07-27 18:10:01 -0700

    first, purge all model-related code

--- edward/models/__init__.py
+++
@@ -1,6 +0,0 @@
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-
-from edward.models.models import *
-from edward.models.random_variables import *
