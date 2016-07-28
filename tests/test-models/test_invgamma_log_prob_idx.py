534f6e38536f671b5c68e06225c51584bfede77c
Alp Kucukelbir <akucukelbir@gmail.com>
2016-07-28 14:19:06 -0400

    updated test-models

--- tests/test-models/test_invgamma_log_prob_idx.py
+++ tests/test-models/test_invgamma_log_prob_idx.py
@@ -9,26 +9,25 @@ import tensorflow as tf
 from edward.models import InvGamma
 from scipy import stats
 
-sess = tf.Session()
 ed.set_seed(98765)
-
 
 def _test(shape, n):
     rv = InvGamma(shape, alpha=tf.zeros(shape)+0.5, beta=tf.zeros(shape)+0.5)
     rv_sample = rv.sample(n)
-    with sess.as_default():
-        x = rv_sample.eval()
-        x_tf = tf.constant(x, dtype=tf.float32)
-        alpha = rv.alpha.eval()
-        beta = rv.beta.eval()
-        for idx in range(shape[0]):
-            assert np.allclose(
-                rv.log_prob_idx((idx, ), x_tf).eval(),
-                stats.invgamma.logpdf(x[:, idx], alpha[idx], scale=beta[idx]))
+    x = rv_sample.eval()
+    x_tf = tf.constant(x, dtype=tf.float32)
+    alpha = rv.alpha.eval()
+    beta = rv.beta.eval()
+    for idx in range(shape[0]):
+        assert np.allclose(
+            rv.log_prob_idx((idx, ), x_tf).eval(),
+            stats.invgamma.logpdf(x[:, idx], alpha[idx], scale=beta[idx]))
 
+class test_invgamma_log_prob_idx_class(tf.test.TestCase):
 
-def test_1d():
-    _test((1, ), 1)
-    _test((1, ), 5)
-    _test((5, ), 1)
-    _test((5, ), 5)
+    def test_1d(self):
+        with self.test_session():
+            _test((1, ), 1)
+            _test((1, ), 5)
+            _test((5, ), 1)
+            _test((5, ), 5)
