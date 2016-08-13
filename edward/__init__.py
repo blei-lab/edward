17ef9b2f5748247578ce79e0207b3b8ad8488c31
Dustin Tran <dustinviettran@gmail.com>
2016-08-13 12:35:43 -0700

    purge codebase for modeling with a metagraph above tensorflow's

--- edward/__init__.py
+++ edward/__init__.py
@@ -10,7 +10,6 @@ from edward import util
 
 # Direct imports for convenience
 from edward.models import PyMC3Model, PythonModel, StanModel
-from edward.models.operations import *
 from edward.criticisms import evaluate, ppc
 from edward.inferences import Inference, MonteCarlo, VariationalInference, MFVI, KLpq, MAP, Laplace
 from edward.util import cumprod, dot, get_dims, get_session, hessian, kl_multivariate_normal, log_sum_exp, logit, multivariate_rbf, rbf, set_seed, softplus, to_simplex
