7b32389f2d696c1410244d48fc5725993567fd7a
Dustin Tran <dustinviettran@gmail.com>
2016-09-25 20:06:00 -0400

    add MetropolisHastings inference algorithm

--- edward/inferences/__init__.py
+++ edward/inferences/__init__.py
@@ -6,5 +6,6 @@ from edward.inferences.inference import *
 from edward.inferences.klpq import *
 from edward.inferences.klqp import *
 from edward.inferences.map import *
+from edward.inferences.metropolis_hastings import *
 from edward.inferences.monte_carlo import *
 from edward.inferences.variational_inference import *
