ac21cc125cbb4efbbef1019673d23af8e0066b1a
Adji Bousso Dieng <diengadji@gmail.com>
2016-10-08 12:39:56 -0400

    added cholesky factor to log_prior computation with control on positive definiteness of the kernel...added cox 1955 reference

--- examples/cox.py
+++ examples/cox.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 """Cox process model using mean-field variational inference.
-   Ref: Miller et al, 2014.
+   Ref: Cox, 1955 and Miller et al, 2014.
 """
 from __future__ import absolute_import
 from __future__ import division
@@ -22,7 +22,7 @@ class CoxProcess:
   z = (z_1, ..., z_N)...each z_i is the intensity function for x_i
 
   p(x, z) = \prod_{i=1}^{N} p(x_i | z_i) p(z_i)
-  	
+    
   p(z_i) = N(z_i | 0, K)
   p(x_i | z_i) = \prod_{v=1}^{V} p(x_{i,v} | z_{i,v})
   p(x_{i,v} | z_{i,v}) = Poisson(x_{i,v} | exp(z_{i,v} + f_0)) 
@@ -34,7 +34,7 @@ class CoxProcess:
   N : int
     Number of data points.
   V : int
-  	Size of the set counts for each data point.
+    Size of the set counts for each data point.
   sigma : float, optional
     Signal variance parameter.
   l : float, optional
@@ -50,65 +50,70 @@ class CoxProcess:
     self.inverse_link = tf.exp
 
   def kernel_row(self, x):
-  	"""
-  	computes the covariance matrix using rbf for the row vector x
-  	Args:
-  	x: a row vector fom the data
-  	   size: V
-  	Returns:
-  	   mat: a matrix pf size VxV
-  	"""
-  	mat = []
-	for i in range(self.V):
-	  vect = []
-	  xi = x[i]
-	  for j in range(self.V):
-	    if j == i:
-	      vect.append(multivariate_rbf(xi, xi, self.sigma, self.l))
-	    else:
-	      xj = x[j]
-	      vect.append(multivariate_rbf(xi, xj, self.sigma, self.l))
+    """
+    computes the covariance matrix using rbf for the row vector x
+    Args:
+    x: a row vector fom the data
+       size: V
+    Returns:
+       mat: a matrix pf size VxV
+    """
+    mat = []
+    for i in range(self.V):
+      vect = []
+      xi = x[i]
+      for j in range(self.V):
+        if j == i:
+          vect.append(multivariate_rbf(xi, xi, self.sigma, self.l))
+        else:
+          xj = x[j]
+          vect.append(multivariate_rbf(xi, xj, self.sigma, self.l))
 
-	  mat.append(vect)
+      mat.append(vect)
 
-	mat = tf.pack(mat)
+    mat = tf.pack(mat)
+    #ensure this is positive definite
+    mat = mat + tf.diag(
+          tf.convert_to_tensor([1e-6, 1e-6], dtype=tf.float32))
 
-	return mat
+    return mat
 
   def kernel(self, x):
-  	"""
-  	wrapper around kernel_row that computes K for x
-  	Args:
-  	x: the whole data
-  	   size: NxV
-  	Returns:
-  	K: the kernel matrix for the whole data
-  	   size: NxVxV
-  	"""
-  	K = [] 	
-	for k in range(self.N):
-		xk = x[k, :]
-		K.append(self.kernel_row(xk))
+    """
+    wrapper around kernel_row that computes K for x
+    Args:
+    x: the whole data
+       size: NxV
+    Returns:
+    K: the kernel matrix for the whole data
+       size: NxVxV
+    """
+    K = []  
+    for k in range(self.N):
+      xk = x[k, :]
+      K.append(self.kernel_row(xk))
 
-	K = tf.pack(K)
+    K = tf.pack(K)
 
-	return K
+    return K
 
   def log_prob(self, xs, zs):
     """
     Args:
     xs: the data
-    	size NxV
+      size NxV
     zs: latent variables
-    	size NxV
-    Returns: log joint log p(xs, zs), scalar	
+      size NxV
+    Returns: log joint log p(xs, zs), scalar  
     """
     x = xs['x']
     z = zs['z']
     z = tf.reshape(z, [self.N, self.V])
 
+    chol = tf.cholesky(self.kernel(x))
+
     log_prior = multivariate_normal_cholesky.logpdf(
-			z, tf.zeros([self.N, self.V]), self.kernel(x))
+      z, tf.zeros([self.N, self.V]), chol)
 
     log_lik = poisson.logpmf(x, mu=self.inverse_link(z))
     log_lik = tf.reduce_sum(log_lik, 1)
@@ -116,20 +121,20 @@ class CoxProcess:
     return tf.reduce_sum(log_prior + log_lik)
     
 def build_toy_dataset(N, V):
-	"""
-	toy data...Identity kernel for GP
-	"""
-	K = np.identity(V)
-	x = np.zeros([N, V])
-	for i in range(N):
-		z_i = mvn.rvs(cov=K, size=1)
-		for v in range(V):
-			lam = np.exp(z_i[v])
-			x[i, v] = poi.rvs(mu=lam, size=1)
+  """
+  toy data...Identity kernel for GP
+  """
+  K = np.identity(V)
+  x = np.zeros([N, V])
+  for i in range(N):
+    z_i = mvn.rvs(cov=K, size=1)
+    for v in range(V):
+      lam = np.exp(z_i[v])
+      x[i, v] = poi.rvs(mu=lam, size=1)
 
-	print("toy data: {}".format(x))
+  print("toy data: {}".format(x))
 
-	return x
+  return x
 
 ed.set_seed(42)
 sess = ed.get_session()
@@ -153,6 +158,7 @@ n_iter = 5000
 for t in range(inference.n_iter):
   info_dict = inference.update()
   if t % inference.n_print == 0:
-  	mean, std = sess.run([qz.mu, qz.sigma])
-  	print("iter: {}...loss: {}...mean: {}...std: {}".format(
-  			info_dict['t'], info_dict['loss'], mean, std))
+    mean, std = sess.run([qz.mu, qz.sigma])
+    print("iter: {}...loss: {}...mean: {}...std: {}".format(
+        info_dict['t'], info_dict['loss'], mean, std))
+    
