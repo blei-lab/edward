f1f34b88d8d070a731765627daf680c412b9ee07
John Pearson <jmxpearson@gmail.com>
2016-11-08 16:25:12 -0500

    fixing tests

--- tests/test-models/test_random_variable_init_value.py
+++ tests/test-models/test_random_variable_init_value.py
@@ -16,7 +16,7 @@ def _test(RV, value, *args, **kwargs):
   value_shape = rv._value.get_shape().as_list()
   expected_shape = (rv.get_batch_shape().as_list() +
                     rv.get_event_shape().as_list())
-  assert value_shape == 0
+  assert value_shape == expected_shape 
   assert rv.dtype == rv._value.dtype
 
 
@@ -29,20 +29,19 @@ def _test_copy(RV, value, *args, **kwargs):
 
 
 class test_random_variable_init_value_class(tf.test.TestCase):
-  def check_shape_and_dtype(self):
+  def test_shape_and_dtype(self):
     with self.test_session():
       _test(Normal, 2, mu=0.5, sigma=1.)
       _test(Normal, [2], mu=[0.5], sigma=[1.])
       _test(Poisson, 2, lam=0.5)
 
-  def check_mismatch_raises(self):
+  def test_mismatch_raises(self):
     with self.test_session():
-        with pytest.raises(ValueError):
-          _test(Normal, 2, mu=[0.5, 0.5], sigma=1.)
-          _test(Normal, 2, mu=[0.5], sigma=[1.])
-          _test(Normal, [2], mu=0.5, sigma=1.)
+      self.assertRaises(ValueError, _test, Normal, 2, mu=[0.5, 0.5], sigma=1.)
+      self.assertRaises(ValueError, _test, Normal, 2, mu=[0.5], sigma=[1.])
+      self.assertRaises(ValueError, _test, Normal, [2], mu=0.5, sigma=1.)
 
-  def check_copy(self):
+  def test_copy(self):
     with self.test_session():
       _test_copy(Normal, 2, mu=0.5, sigma=1.)
       _test_copy(Normal, [2], mu=[0.5], sigma=[1.])
