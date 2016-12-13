82e534dcc416b26d1e33d077f5c538900390db50
Dustin Tran <dustinviettran@gmail.com>
2016-12-13 01:16:38 -0500

    prevent users from installing edward with incompatible tf versions

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     author_email="dustin@cs.columbia.edu",
     packages=['edward', 'edward.criticisms', 'edward.inferences',
               'edward.models', 'edward.stats', 'edward.util'],
-    install_requires=['tensorflow>=0.11.0rc0',
+    install_requires=['tensorflow>=0.11.0rc0,!=0.12.0rc0,!=0.12.0rc1',
                       'numpy>=1.7',
                       'six>=1.10.0'],
     extras_require={'stan': ['pystan>=2.0.1.3'],
