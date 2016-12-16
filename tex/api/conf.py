113c416c9381c65576a5b892d1d7ac90dd0e58d0
Dustin Tran <dustinviettran@gmail.com>
2016-12-16 13:27:29 -0500

    Update documentation

--- tex/api/conf.py
+++ tex/api/conf.py
@@ -37,6 +37,7 @@ extensions = [
 ]
 
 autoclass_content = 'both'
+autodoc_member_order = 'bysource'
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
