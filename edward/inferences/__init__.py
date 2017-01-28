71641626b758fb21105180c687a05a7b8f303959
David Wadden <dave.wadden@gmail.com>
2017-01-28 12:08:47 -0800

    [WIP] Added implementation of SGHMC and example. (#415)
    * Added implementation of SGHMC and example.
    
    Figured out how to retrieve MCMC trace more easily.
    
    Clean up normal_sghmc
    
    - Make indent 2 spaces instead of 4
    - Get rid of TODO's.
    
    Clean up sghmc.py
    
    Get rid of TODO's.
    
    * Fix pep8 style violations.
    
    * Add a blank line to make flake8 happy.
    
    * Example: Bayesian linear regression with SGHMC.

--- edward/inferences/__init__.py
+++ edward/inferences/__init__.py
@@ -10,4 +10,5 @@ from edward.inferences.map import *
 from edward.inferences.metropolis_hastings import *
 from edward.inferences.monte_carlo import *
 from edward.inferences.sgld import *
+from edward.inferences.sghmc import *
 from edward.inferences.variational_inference import *
