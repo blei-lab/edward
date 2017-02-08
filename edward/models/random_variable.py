de28deaccfb540ade653bbc4e5df3bae918ff8ea
Dustin Tran <dustinviettran@gmail.com>
2017-02-08 17:02:09 -0500

    add operator overloading to RandomVariable (#445)
    * add operator overloading, following tf.Tensor
    
    * add unit test

--- edward/models/random_variable.py
+++ edward/models/random_variable.py
@@ -88,6 +88,97 @@ class RandomVariable(object):
   def __repr__(self):
     return self.__str__()
 
+  def __add__(self, other):
+    return tf.add(self, other)
+
+  def __radd__(self, other):
+    return tf.add(other, self)
+
+  def __sub__(self, other):
+    return tf.subtract(self, other)
+
+  def __rsub__(self, other):
+    return tf.subtract(other, self)
+
+  def __mul__(self, other):
+    return tf.multiply(self, other)
+
+  def __rmul__(self, other):
+    return tf.multiply(other, self)
+
+  def __div__(self, other):
+    return tf.div(self, other)
+
+  __truediv__ = __div__
+
+  def __rdiv__(self, other):
+    return tf.div(other, self)
+
+  __rtruediv__ = __rdiv__
+
+  def __floordiv__(self, other):
+    return tf.floor(tf.div(self, other))
+
+  def __rfloordiv__(self, other):
+    return tf.floor(tf.div(other, self))
+
+  def __mod__(self, other):
+    return tf.mod(self, other)
+
+  def __rmod__(self, other):
+    return tf.mod(other, self)
+
+  def __lt__(self, other):
+    return tf.less(self, other)
+
+  def __le__(self, other):
+    return tf.less_equal(self, other)
+
+  def __gt__(self, other):
+    return tf.greater(self, other)
+
+  def __ge__(self, other):
+    return tf.greater_equal(self, other)
+
+  def __and__(self, other):
+    return tf.logical_and(self, other)
+
+  def __rand__(self, other):
+    return tf.logical_and(other, self)
+
+  def __or__(self, other):
+    return tf.logical_or(self, other)
+
+  def __ror__(self, other):
+    return tf.logical_or(other, self)
+
+  def __xor__(self, other):
+    return tf.logical_xor(self, other)
+
+  def __rxor__(self, other):
+    return tf.logical_xor(other, self)
+
+  def __pow__(self, other):
+    return tf.pow(self, other)
+
+  def __rpow__(self, other):
+    return tf.pow(other, self)
+
+  def __invert__(self):
+    return tf.logical_not(self)
+
+  def __neg__(self):
+    return tf.negative(self)
+
+  def __abs__(self):
+    return tf.abs(self)
+
+  def __hash__(self):
+    return id(self)
+
+  def __eq__(self, other):
+    return id(self) == id(other)
+
   def value(self):
     """Get tensor that the random variable corresponds to."""
     return self._value
