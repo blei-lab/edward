Fixed ds.kl back to kl_multivariate_normal.

edward/inferences/klqp.py
@@ -463,7 +463,7 @@ def build_reparam_kl_loss_and_gradients(inference, var_list):
 
   if inference.model_wrapper is None:
     kl = tf.reduce_sum([inference.kl_scaling.get(z, 1.0) *
-                        tf.reduce_sum(ds.kl(qz, z))
+                        tf.reduce_sum(kl_multivariate_normal(qz, z))
                         for z, qz in six.iteritems(inference.latent_vars)])
   else:
     kl = tf.reduce_sum([tf.reduce_sum(kl_multivariate_normal(qz.mu, qz.sigma))
@@ -679,7 +679,7 @@ def build_score_kl_loss_and_gradients(inference, var_list):
 
   if inference.model_wrapper is None:
     kl = tf.reduce_sum([inference.kl_scaling.get(z, 1.0) *
-                        tf.reduce_sum(ds.kl(qz, z))
+                        tf.reduce_sum(kl_multivariate_normal(qz, z))
                         for z, qz in six.iteritems(inference.latent_vars)])
   else:
     kl = tf.reduce_sum([tf.reduce_sum(kl_multivariate_normal(qz.mu, qz.sigma))