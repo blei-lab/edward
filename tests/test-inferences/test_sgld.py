5d4f290ee752da140c77ee6678561bf236eae07e
Alp Kucukelbir <akucukelbir@gmail.com>
2017-02-16 15:25:01 -0500

    found magic combination for SGLD to converge

--- tests/test-inferences/test_sgld.py
+++ tests/test-inferences/test_sgld.py
@@ -18,11 +18,11 @@ class test_sgld_class(tf.test.TestCase):
       mu = Normal(mu=0.0, sigma=1.0)
       x = Normal(mu=tf.ones(50) * mu, sigma=1.0)
 
-      qmu = Empirical(params=tf.Variable(tf.ones([10000])))
+      qmu = Empirical(params=tf.Variable(tf.ones([5000])))
 
       # analytic solution: N(mu=0.0, sigma=\sqrt{1/51}=0.140)
       inference = ed.SGLD({mu: qmu}, data={x: x_data})
-      inference.run()
+      inference.run(step_size=0.2)
 
       self.assertAllClose(qmu.mean().eval(), 0, rtol=1e-2, atol=1e-2)
       self.assertAllClose(qmu.std().eval(), np.sqrt(1 / 51),
