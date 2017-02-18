17c363a37ba28d14b466e0b7f580f6543795c2d9
Dustin Tran <dustinviettran@gmail.com>
2017-02-18 17:57:25 -0500

    add test for scale with tensor

--- tests/test-inferences/test_scale.py
+++ tests/test-inferences/test_scale.py
@@ -3,6 +3,7 @@ from __future__ import division
 from __future__ import print_function
 
 import edward as ed
+import numpy as np
 import tensorflow as tf
 
 from edward.models import Normal
@@ -19,7 +20,7 @@ class NormalNormal:
 
 class test_inference_scale_class(tf.test.TestCase):
 
-  def test_subgraph(self):
+  def test_scale_0d(self):
     N = 10
     M = 5
     mu = Normal(mu=0.0, sigma=1.0)
@@ -32,6 +33,21 @@ class test_inference_scale_class(tf.test.TestCase):
     inference = ed.KLqp({mu: qmu}, data)
     inference.initialize(scale={x: float(N) / M})
     assert inference.scale[x] == float(N) / M
+
+  def test_scale_1d(self):
+    N = 10
+    M = 5
+    mu = Normal(mu=0.0, sigma=1.0)
+    x = Normal(mu=tf.ones(M) * mu, sigma=tf.ones(M))
+
+    qmu = Normal(mu=tf.Variable(0.0), sigma=tf.constant(1.0))
+
+    x_ph = tf.placeholder(tf.float32, [M])
+    data = {x: x_ph}
+    inference = ed.KLqp({mu: qmu}, data)
+    inference.initialize(scale={x: tf.cast(tf.range(M), tf.float32)})
+    with self.test_session():
+      self.assertAllClose(inference.scale[x].eval(), np.arange(M))
 
   def test_minibatch(self):
     N = 10
