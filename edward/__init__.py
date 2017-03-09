6c0de57e962e92793edf45978d9641f0baa196ca
Lyndon Ollar <lbollar@gmail.com>
2017-03-09 10:04:18 -0600

    Added helper function to calculate optimal control variate coefficient (#513)
    * Added helper function to calculate optimal control variate coefficient
    
    * added blank line for pep
    
    * Moved get_control_variate_coef to util/tensorflow
      * Deleted control variate file in inferences and moved function to
        tensorflow file in util
    
      * Renamed get_control_variate_scalar to get_control_variate_coef
    
      * Created test file for function and implemented test.
    
    * added newline
    
    * Added get_control_variate_coef to __init__.py

--- edward/__init__.py
+++ edward/__init__.py
@@ -15,7 +15,7 @@ from edward.inferences import Inference, MonteCarlo, VariationalInference, \
     GANInference, WGANInference, ImplicitKLqp, MAP, Laplace
 from edward.models import RandomVariable
 from edward.util import copy, dot, get_ancestors, get_children, \
-    get_descendants, get_dims, get_parents, get_session, get_siblings, \
-    get_variables, logit, multivariate_rbf, random_variables, rbf, \
-    reduce_logmeanexp, set_seed, to_simplex
+    get_control_variate_coef, get_descendants, get_dims, get_parents, \
+    get_session, get_siblings, get_variables, logit, multivariate_rbf, \
+    random_variables, rbf, reduce_logmeanexp, set_seed, to_simplex
 from edward.version import __version__
