ea59656e7e98003a745ff02173e50514927eb361
Alp Kucukelbir <akucukelbir@gmail.com>
2017-03-14 11:06:37 -0400

    addressing dustin's inline comments

--- tests/test-notebooks/test_notebooks.py
+++ tests/test-notebooks/test_notebooks.py
@@ -2,20 +2,21 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+import glob
 import nbformat
+import os
+import sys
+import time
+import traceback
+import unittest
+
 from nbconvert.preprocessors import ExecutePreprocessor
 from nbconvert.preprocessors.execute import CellExecutionError
 
-import glob
-import traceback
-import unittest
-import sys
-import time
-import os
 
+class test_notebooks(unittest.TestCase):
 
-class TestNotebooks(unittest.TestCase):
-  def _execNotebook(self, ep, notebook_filename, nbpath):
+  def _exec_notebook(self, ep, notebook_filename, nbpath):
     with open(notebook_filename) as f:
       nb = nbformat.read(f, as_version=nbformat.current_nbformat)
       try:
@@ -29,8 +30,8 @@ class TestNotebooks(unittest.TestCase):
                         % notebook_filename)
 
   def test_all_notebooks(self):
-    ''' Test all notebooks except blacklist. '''
-    blacklist = ['gan.ipynb', 'iclr2017.ipynb']
+    """ Test all notebooks except blacklist. """
+    blacklist = ['gan.ipynb']
     pythonkernel = 'python' + str(sys.version_info[0])
     nbpath = '../../docs/notebooks/'
     # see http://nbconvert.readthedocs.io/en/stable/execute_api.html
@@ -41,9 +42,8 @@ class TestNotebooks(unittest.TestCase):
     for notebook_filename in lfiles:
       if(os.path.basename(notebook_filename) not in blacklist):
         t = time.time()
-        self._execNotebook(ep, notebook_filename, nbpath)
+        self._exec_notebook(ep, notebook_filename, nbpath)
         print(notebook_filename, 'took %g seconds.' % (time.time() - t))
-
 
 if __name__ == '__main__':
   unittest.main()
