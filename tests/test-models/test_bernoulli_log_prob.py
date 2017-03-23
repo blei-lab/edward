ae7c92f3d5a400bb54558d02d5ff8eb5098bdf18
Dustin Tran <dustinviettran@gmail.com>
2017-03-23 12:52:59 -0400

    Remove tests of contrib.distributions' log_prob/sample (#577)
    * remove tests of contrib.distributions log_prob/sample methods
    
    * add log_prob/sample test for autogenerated RandomVariable
    
    * fix pep8

--- tests/test-models/test_bernoulli_log_prob.py
+++ tests/test-models/test_bernoulli_log_prob.py
@@ -2,33 +2,26 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-import edward as ed
-import numpy as np
 import tensorflow as tf
 
 from edward.models import Bernoulli
-from scipy import stats
-
-
-def _test(p, n):
-  rv = Bernoulli(p=p)
-  rv_sample = rv.sample(n)
-  x = rv_sample.eval()
-  x_tf = tf.constant(x, dtype=tf.float32)
-  p = p.eval()
-  assert np.allclose(rv.log_prob(x_tf).eval(),
-                     stats.bernoulli.logpmf(x, p))
+from tensorflow.contrib import distributions as ds
 
 
 class test_bernoulli_log_prob_class(tf.test.TestCase):
 
+  def _test(self, p, n):
+    rv = Bernoulli(p=p)
+    dist = ds.Bernoulli(p=p)
+    x = rv.sample(n).eval()
+    self.assertAllEqual(rv.log_prob(x).eval(), dist.log_prob(x).eval())
+
   def test_1d(self):
-    ed.set_seed(98765)
     with self.test_session():
-      _test(tf.zeros([1]) + 0.5, [1])
-      _test(tf.zeros([1]) + 0.5, [5])
-      _test(tf.zeros([5]) + 0.5, [1])
-      _test(tf.zeros([5]) + 0.5, [5])
+      self._test(tf.zeros([1]) + 0.5, [1])
+      self._test(tf.zeros([1]) + 0.5, [5])
+      self._test(tf.zeros([5]) + 0.5, [1])
+      self._test(tf.zeros([5]) + 0.5, [5])
 
 if __name__ == '__main__':
   tf.test.main()
