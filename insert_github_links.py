213c1804531f2dfdd9caabbf3ca24728807a66ad
Dustin Tran <dustinviettran@gmail.com>
2017-03-25 02:52:49 -0500

    Update documentation

--- insert_github_links.py
+++ insert_github_links.py
@@ -13,14 +13,9 @@ from bs4 import BeautifulSoup
 
 def find_filename_and_line(method_name):
   components = string.split(method_name, '.')
-  if len(components) == 3:
-    method_obj = getattr(getattr(edward, components[1]), components[2])
-  elif len(components) == 4:
-    method_obj = getattr(getattr(getattr(edward, components[1]),
-                                 components[2]),
-                         components[3])
-  else:
-    return '', 0
+  method_obj = edward
+  for i in range(1, len(components)):
+    method_obj = getattr(method_obj, components[i])
 
   rel_path = inspect.getsourcefile(method_obj).split("edward/", 1)[1]
   line_no = inspect.getsourcelines(method_obj)[-1]
@@ -34,12 +29,16 @@ for filename in filenames:
   soup = BeautifulSoup(open(filename), 'html.parser')
   github = "https://github.com/blei-lab/edward/blob/master/"
   for a in soup.find_all("a", "headerlink"):
-    rel_path, line_no = find_filename_and_line(a['href'][1:])
-    link = github + rel_path + '#L' + str(line_no)
-    a['href'] = link
-    a['class'] = 'u-pull-right'
-    a['title'] = "Link to definition on GitHub."
-    a.string.replace_with("[source]")
+    try:
+      rel_path, line_no = find_filename_and_line(a['href'][1:])
+      link = github + rel_path + '#L' + str(line_no)
+      a['href'] = link
+      a['class'] = 'u-pull-right'
+      a['title'] = "Link to definition on GitHub."
+      a.string.replace_with("[source]")
+    except:  # e.g., if method_obj is a property
+      a['href'] = ""
+      a.string.replace_with("")
 
   html = str(soup)
   with open(filename, 'wb') as file:
