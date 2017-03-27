ebf1de83241e857af4fc0dbf2a21331f0ba012ca
Dustin Tran <dustinviettran@gmail.com>
2017-03-27 14:27:58 -0400

    speed up gp classification; remove multivariate_rbf, improve rbf

--- tests/test-util/test_rbf.py
+++ tests/test-util/test_rbf.py
@@ -10,137 +10,82 @@ from edward.util import rbf
 
 class test_rbf_class(tf.test.TestCase):
 
-  def test_rbf_0d(self):
+  def test_x(self):
     with self.test_session():
-      x = tf.constant(0.0)
-      self.assertAllClose(rbf(x).eval(),
-                          1.0)
-      x = tf.constant(10.0)
-      y = tf.constant(2.0)
-      self.assertAllClose(rbf(x, y=y).eval(),
-                          1.26e-14)
-      x = tf.constant(0.0)
-      y = tf.constant(1.0)
-      sigma = tf.constant(10.0)
-      self.assertAllClose(rbf(x, y=y, sigma=sigma).eval(),
-                          60.6530685)
-      x = tf.constant(0.0)
-      y = tf.constant(1.0)
-      sigma = tf.constant(10.0)
-      l = tf.constant(5.0)
-      self.assertAllClose(rbf(x, y=y, sigma=sigma, l=l).eval(),
-                          98.01986694)
+      X = tf.constant([[0.0], [0.0]])
+      X2 = tf.constant([[0.0], [0.0]])
+      self.assertAllClose(rbf(X).eval(),
+                          [[1.0, 1.0], [1.0, 1.0]])
+      self.assertAllClose(rbf(X, X2).eval(),
+                          [[1.0, 1.0], [1.0, 1.0]])
 
-  def test_rbf_1d(self):
+  def test_x2(self):
     with self.test_session():
-      x = tf.constant([0.0])
-      self.assertAllClose(rbf(x).eval(),
-                          [1.0])
-      x = tf.constant([10.0])
-      y = tf.constant([2.0])
-      self.assertAllClose(rbf(x, y=y).eval(),
-                          [1.26e-14])
-      x = tf.constant([0.0])
-      y = tf.constant([1.0])
-      sigma = tf.constant(10.0)
-      self.assertAllClose(rbf(x, y=y, sigma=sigma).eval(),
-                          [60.6530685])
-      x = tf.constant([0.0])
-      y = tf.constant([1.0])
-      sigma = tf.constant(10.0)
-      l = tf.constant(5.0)
-      self.assertAllClose(rbf(x, y=y, sigma=sigma, l=l).eval(),
-                          [98.01986694])
-      x = tf.constant([0.0, 1.0])
-      self.assertAllClose(rbf(x).eval(),
-                          [1.0, 0.606530666])
-      x = tf.constant([10.0, 3.0])
-      y = tf.constant([2.0, 3.0])
-      self.assertAllClose(rbf(x, y=y).eval(),
-                          [1.266417e-14, 1.0])
-      x = tf.constant([0.0, 1.0])
-      y = tf.constant([1.0, 2.0])
-      sigma = tf.constant(10.0)
-      self.assertAllClose(rbf(x, y=y, sigma=sigma).eval(),
-                          [60.6530685, 60.6530685])
-      x = tf.constant([0.0, -23.0])
-      y = tf.constant([1.0, -93.0])
-      sigma = tf.constant(10.0)
-      l = tf.constant(50.0)
-      self.assertAllClose(rbf(x, y=y, sigma=sigma, l=l).eval(),
-                          [99.980003, 37.531109])
+      X = tf.constant([[10.0], [2.0]])
+      X2 = tf.constant([[2.0], [10.0]])
+      self.assertAllClose(rbf(X, X2).eval(),
+                          [[1.266417e-14, 1.0], [1.0, 1.266417e-14]])
+      self.assertAllClose(rbf(X2, X).eval(),
+                          [[1.266417e-14, 1.0], [1.0, 1.266417e-14]])
 
-  def test_rbf_2d(self):
-    with self.test_session():
-      x = tf.constant([[0.0], [0.0]])
-      self.assertAllClose(rbf(x).eval(),
-                          [[1.0], [1.0]])
-      x = tf.constant([[10.0], [2.0]])
-      y = tf.constant([[2.0], [10.0]])
-      self.assertAllClose(rbf(x, y=y).eval(),
-                          [[1.266417e-14], [1.266417e-14]])
-      x = tf.constant([[0.0], [10.0]])
-      y = tf.constant([[1.0], [1.0]])
-      sigma = tf.constant(10.0)
-      self.assertAllClose(rbf(x, y=y, sigma=sigma).eval(),
-                          [[6.065307e+01], [2.576757e-16]])
-      x = tf.constant([[0.0], [10.0]])
-      y = tf.constant([[1.0], [1.0]])
-      sigma = tf.constant(10.0)
-      l = tf.constant(5.0)
-      self.assertAllClose(rbf(x, y=y, sigma=sigma, l=l).eval(),
-                          [[98.019867], [19.789869]])
-      x = tf.constant([[10.0, 3.0], [10.0, 3.0]])
-      self.assertAllClose(rbf(x, y=y).eval(),
-                          [[2.576757e-18, 1.353353e-01],
-                           [2.576757e-18, 1.353353e-01]])
-      x = tf.constant([[10.0, 3.0], [10.0, 3.0]])
-      y = tf.constant([[2.0, 3.0], [2.0, 3.0]])
-      self.assertAllClose(rbf(x, y=y).eval(),
-                          [[1.266417e-14, 1.0],
-                           [1.266417e-14, 1.0]])
-      x = tf.constant([[0.0, 1.0], [10.0, -3.0]])
-      y = tf.constant([[1.0, 2.0], [1.0, 2.0]])
-      sigma = tf.constant(10.0)
-      self.assertAllClose(rbf(x, y=y, sigma=sigma).eval(),
-                          [[6.065307e+01, 6.065307e+01],
-                           [2.576757e-16, 3.726653e-04]])
-      x = tf.constant([[10.0, 3.0], [10.0, 3.0]])
-      y = tf.constant([[2.0, 3.0], [2.0, 3.0]])
-      sigma = tf.constant(10.0)
-      l = tf.constant(5.0)
-      self.assertAllClose(rbf(x, y=y, sigma=sigma, l=l).eval(),
-                          [[27.80373, 100], [27.80373, 100]])
+      X = tf.constant([[2.0, 2.5], [4.1, 5.0]])
+      X2 = tf.constant([[1.5, 2.0], [3.1, 4.2]])
+      self.assertAllClose(rbf(X, X2).eval(),
+                          [[0.778800, 0.128734],
+                           [0.000378, 0.440431]], atol=1e-5, rtol=1e-5)
 
-  def test_contraint_raises(self):
+  def test_lengthscale(self):
+    """checked calculations by hand, e.g.,
+    np.exp(-((2.0 - 1.5)**2 / (2.0**2) + (2.5 - 2.0)**2 / (1.5**2)) / 2)
+    np.exp(-((2.0 - 3.1)**2 / (2.0**2) + (2.5 - 4.2)**2 / (1.5**2)) / 2)
+    np.exp(-((4.1 - 1.5)**2 / (2.0**2) + (5.0 - 2.0)**2 / (1.5**2)) / 2)
+    np.exp(-((4.1 - 3.1)**2 / (2.0**2) + (5.0 - 4.2)**2 / (1.5**2)) / 2)
+    """
     with self.test_session():
-      x = tf.constant(0.0)
-      y = tf.constant(1.0)
-      sigma = tf.constant(-1.0)
-      l = tf.constant(-5.0)
+      X = tf.constant([[2.0, 2.5], [4.1, 5.0]])
+      X2 = tf.constant([[1.5, 2.0], [3.1, 4.2]])
+      lengthscale1 = tf.constant(2.0)
+      lengthscale2 = tf.constant([2.0, 2.0])
+      lengthscale3 = tf.constant([2.0, 1.5])
+      self.assertAllClose(rbf(X, X2, lengthscale1).eval(),
+                          [[0.939413, 0.598996],
+                           [0.139456, 0.814647]], atol=1e-5, rtol=1e-5)
+      self.assertAllClose(rbf(X, X2, lengthscale2).eval(),
+                          [[0.939413, 0.598996],
+                           [0.139456, 0.814647]], atol=1e-5, rtol=1e-5)
+      self.assertAllClose(rbf(X, X2, lengthscale3).eval(),
+                          [[0.916855, 0.452271],
+                           [0.058134, 0.765502]], atol=1e-5, rtol=1e-5)
+
+  def test_variance(self):
+    with self.test_session():
+      X = tf.constant([[2.0, 2.5], [4.1, 5.0]])
+      X2 = tf.constant([[1.5, 2.0], [3.1, 4.2]])
+      variance = tf.constant(1.4)
+      self.assertAllClose(rbf(X, X2, variance=variance).eval(),
+                          [[1.090321, 0.180228],
+                           [0.000529, 0.616604]], atol=1e-5, rtol=1e-5)
+
+  def test_all(self):
+    with self.test_session():
+      X = tf.constant([[2.0, 2.5], [4.1, 5.0]])
+      X2 = tf.constant([[1.5, 2.0], [3.1, 4.2]])
+      lengthscale = tf.constant([2.0, 1.5])
+      variance = tf.constant(1.4)
+      self.assertAllClose(rbf(X, X2, lengthscale, variance).eval(),
+                          [[1.283597, 0.633180],
+                           [0.081387, 1.071704]], atol=1e-5, rtol=1e-5)
+
+  def test_raises(self):
+    with self.test_session():
+      X1 = tf.constant([[0.0]])
+      X2 = tf.constant([[0.0]])
+      lengthscale = tf.constant(-5.0)
+      variance = tf.constant(-1.0)
       with self.assertRaisesOpError('Condition'):
-        rbf(x, y=y, sigma=sigma).eval()
-        rbf(x, y=y, l=l).eval()
-        rbf(x, y=y, sigma=sigma, l=l).eval()
-      x = np.inf * tf.constant(1.0)
-      y = tf.constant(1.0)
-      sigma = tf.constant(1.0)
-      l = tf.constant(5.0)
-      with self.assertRaisesOpError('Inf'):
-        rbf(x).eval()
-        rbf(x, y=y).eval()
-        rbf(x, y=y, sigma=sigma).eval()
-        rbf(x, y=y, l=l).eval()
-        rbf(x, y=y, sigma=sigma, l=l).eval()
-      x = tf.constant(0.0)
-      y = np.nan * tf.constant(1.0)
-      sigma = tf.constant(1.0)
-      l = tf.constant(5.0)
-      with self.assertRaisesOpError('NaN'):
-        rbf(x, y=y).eval()
-        rbf(x, y=y, sigma=sigma).eval()
-        rbf(x, y=y, l=l).eval()
-        rbf(x, y=y, sigma=sigma, l=l).eval()
+        rbf(X1, X2, variance=variance).eval()
+        rbf(X1, X2, lengthscale).eval()
+        rbf(X1, X2, lengthscale, variance).eval()
 
 if __name__ == '__main__':
   tf.test.main()
