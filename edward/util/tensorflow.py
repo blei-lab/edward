0f3f5348eee531e7ca60c6faf7699b683e3cbe13
Dustin Tran <dustinviettran@gmail.com>
2017-03-29 19:52:53 -0400

    Speed up utility function for RBF (#596)
    * speed up gp classification; remove multivariate_rbf, improve rbf
    
    * fix pep8

--- edward/util/tensorflow.py
+++ edward/util/tensorflow.py
@@ -76,96 +76,61 @@ def logit(x):
   return tf.log(x) - tf.log(1.0 - x)
 
 
-def multivariate_rbf(x, y=0.0, sigma=1.0, l=1.0):
-  """Squared-exponential kernel
+def rbf(X, X2=None, lengthscale=1.0, variance=1.0):
+  """Radial basis function kernel, also known as the squared
+  exponential or exponentiated quadratic. It is defined as
 
-  .. math:: k(x, y) = \sigma^2 \exp{ -1/(2l^2) \sum_i (x_i - y_i)^2 }
+  .. math::
 
-  Parameters
-  ----------
-  x : tf.Tensor
-    A n-D tensor.
-  y : tf.Tensor, optional
-    A tensor of same shape as ``x``.
-  sigma : tf.Tensor, optional
-    A 0-D tensor, representing the standard deviation of radial
-    basis function.
-  l : tf.Tensor, optional
-    A 0-D tensor, representing the lengthscale of radial basis
-    function.
+    k(x, x') = \\sigma^2 \exp\Big(
+        -\frac{1}{2} \sum_{d=1}^D \frac{1}{\\ell_d^2} (x_d - x'_d)^2 \Big)
 
-  Returns
-  -------
-  tf.Tensor
-    A tensor of one less dimension than the input.
+  for output variance :math:`\\sigma^2` and lengthscale :math:`\\ell^2`.
 
-  Raises
-  ------
-  InvalidArgumentError
-    If the mean variables have Inf or NaN values, or if the scale
-    and length variables are not positive.
-  """
-  x = tf.convert_to_tensor(x)
-  y = tf.convert_to_tensor(y)
-  sigma = tf.convert_to_tensor(sigma)
-  l = tf.convert_to_tensor(l)
-  dependencies = [tf.verify_tensor_all_finite(x, msg=''),
-                  tf.verify_tensor_all_finite(y, msg=''),
-                  tf.assert_positive(sigma),
-                  tf.assert_positive(l)]
-  x = control_flow_ops.with_dependencies(dependencies, x)
-  y = control_flow_ops.with_dependencies(dependencies, y)
-  sigma = control_flow_ops.with_dependencies(dependencies, sigma)
-  l = control_flow_ops.with_dependencies(dependencies, l)
-
-  return tf.pow(sigma, 2.0) * \
-      tf.exp(-1.0 / (2.0 * tf.pow(l, 2.0)) * tf.reduce_sum(tf.pow(x - y, 2.0)))
-
-
-def rbf(x, y=0.0, sigma=1.0, l=1.0):
-  """Squared-exponential kernel element-wise
-
-  .. math:: k(x, y) = \sigma^2 \exp{ -1/(2l^2) (x - y)^2 }
+  The kernel is evaluated over all pairs of rows, ``k(X[i, ], X2[j, ])``.
+  If ``X2`` is not specified, then it evaluates over all pairs
+  of rows in ``X``, ``k(X[i, ], X[j, ])``. The output is a matrix
+  where each entry (i, j) is the kernel over the ith and jth rows.
 
   Parameters
   ----------
-  x : tf.Tensor
-    A n-D tensor.
-  y : tf.Tensor, optional
-    A tensor of same shape as ``x``.
-  sigma : tf.Tensor, optional
-    A 0-D tensor, representing the standard deviation of radial
-    basis function.
-  l : tf.Tensor, optional
-    A 0-D tensor, representing the lengthscale of radial basis
-    function.
+  X : tf.Tensor
+    N x D matrix of N data points each with D features.
+  X2 : tf.Tensor, optional
+    N x D matrix of N data points each with D features.
+  lengthscale : tf.Tensor, optional
+    Lengthscale parameter, a positive scalar or D-dimensional vector.
+  variance : tf.Tensor, optional
+    Output variance parameter, a positive scalar.
 
-  Returns
-  -------
-  tf.Tensor
-    A tensor of one less dimension than the input.
-
-  Raises
-  ------
-  InvalidArgumentError
-    If the mean variables have Inf or NaN values, or if the scale
-    and length variables are not positive.
+  Examples
+  --------
+  >>> X = tf.random_normal([100, 5])
+  >>> K = ed.rbf(X)
+  >>> assert K.shape == (100, 100)
   """
-  x = tf.convert_to_tensor(x)
-  y = tf.convert_to_tensor(y)
-  sigma = tf.convert_to_tensor(sigma)
-  l = tf.convert_to_tensor(l)
-  dependencies = [tf.verify_tensor_all_finite(x, msg=''),
-                  tf.verify_tensor_all_finite(y, msg=''),
-                  tf.assert_positive(sigma),
-                  tf.assert_positive(l)]
-  x = control_flow_ops.with_dependencies(dependencies, x)
-  y = control_flow_ops.with_dependencies(dependencies, y)
-  sigma = control_flow_ops.with_dependencies(dependencies, sigma)
-  l = control_flow_ops.with_dependencies(dependencies, l)
+  lengthscale = tf.convert_to_tensor(lengthscale)
+  variance = tf.convert_to_tensor(variance)
+  dependencies = [tf.assert_positive(lengthscale),
+                  tf.assert_positive(variance)]
+  lengthscale = control_flow_ops.with_dependencies(dependencies, lengthscale)
+  variance = control_flow_ops.with_dependencies(dependencies, variance)
 
-  return tf.pow(sigma, 2.0) * \
-      tf.exp(-1.0 / (2.0 * tf.pow(l, 2.0)) * tf.pow(x - y, 2.0))
+  X = tf.convert_to_tensor(X)
+  X = X / lengthscale
+  Xs = tf.reduce_sum(tf.square(X), 1)
+  if X2 is None:
+    X2 = X
+    X2s = Xs
+  else:
+    X2 = tf.convert_to_tensor(X2)
+    X2 = X2 / lengthscale
+    X2s = tf.reduce_sum(tf.square(X2), 1)
+
+  square = tf.reshape(Xs, [-1, 1]) + tf.reshape(X2s, [1, -1]) - \
+      2 * tf.matmul(X, X2, transpose_b=True)
+  output = variance * tf.exp(-square / 2)
+  return output
 
 
 def reduce_logmeanexp(input_tensor, axis=None, keep_dims=False):
