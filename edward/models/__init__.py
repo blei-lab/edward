01e6f166c138fe05bdbfd392e10ee182958c44cc
Matt Hoffman <heywhoah@gmail.com>
2017-04-04 16:18:18 -0700

    Feature/param mixture (#592)
    * Added sample_shape parameter to RandomVariable
    
    * ParamMixture class
    
    * Added a test and some statistics
    
    * PEP8 whitespace
    
    * Dumb merge bugfix.
    
    * minor style revisions
    
    * more revisions over test and class
    
    * split up param mixture test
    
    * update sample test; improve validate_args
    
    * fix batch shape
    
    * raise error if stats used for non-scalar batch shape
    
    * properly handle dynamic shapes
    
    * Fixed ParamMixture log_prob(), and restored the dependency between cat and output.
    
    * PEP8 fix
    
    * Added tests for ParamMixture's marginal and conditional probabilities
    
    * Fixes to ParamMixture shape stuff.
    
    * xrange not compatible with python3.

--- edward/models/__init__.py
+++ edward/models/__init__.py
@@ -4,6 +4,7 @@ from __future__ import print_function
 
 from edward.models.dirichlet_process import *
 from edward.models.empirical import *
+from edward.models.param_mixture import *
 from edward.models.point_mass import *
 from edward.models.random_variable import *
 from edward.models.random_variables import *
