cbbc055c1d4a3a86912e15578ad46be5cfa04247
Lyndon Ollar <lbollar@gmail.com>
2017-04-08 13:28:16 -0500

    more pep fixes

--- tests/test-criticisms/test_ppc_plots.py
+++ tests/test-criticisms/test_ppc_plots.py
@@ -12,14 +12,13 @@ class test_ppc_plots_class(tf.test.TestCase):
   def test_ppc_density_plot(self):
     y = np.random.randn(20)
     y_rep = np.random.randn(20, 20)
-  
+
     ppc_density_plot(y, y_rep)
-  
-  
+
   def test_ppc_stat_hist_plot(self):
     y = np.random.randn(20)
     t = 0.0
-  
+
     ppc_stat_hist_plot(t, y, stat_name="mean", bins=10)
 
 if __name__ == '__main__':
