38cf030802aefc0c4f69110557df97ccbb44bad2
Dustin Tran <dustinviettran@gmail.com>
2017-04-11 20:43:33 -0400

    add style cleanup to conjugate_log_probs.py

--- edward/models/conjugate_log_probs.py
+++ edward/models/conjugate_log_probs.py
@@ -5,7 +5,6 @@ from __future__ import print_function
 import numpy as np
 import tensorflow as tf
 
-from edward.models.random_variable import RandomVariable
 from edward.models import random_variables as rvs
 
 
@@ -22,8 +21,8 @@ def _val_wrapper(f):
 def beta_log_prob(self, val):
   a = self.parameters['a']
   b = self.parameters['b']
-  result = (a - 1.) * tf.log(val)
-  result += (b - 1.) * tf.log(tf.constant(1.) - val)
+  result = (a - 1.0) * tf.log(val)
+  result += (b - 1.0) * tf.log(tf.constant(1.0) - val)
   result += -tf.lgamma(a) - tf.lgamma(b) + tf.lgamma(a + b)
   return result
 rvs.Beta.conjugate_log_prob = beta_log_prob
@@ -32,7 +31,7 @@ rvs.Beta.conjugate_log_prob = beta_log_prob
 @_val_wrapper
 def dirichlet_log_prob(self, val):
   alpha = self.parameters['alpha']
-  result = tf.reduce_sum((alpha - 1.) * tf.log(val), -1)
+  result = tf.reduce_sum((alpha - 1.0) * tf.log(val), -1)
   result += tf.reduce_sum(-tf.lgamma(alpha), -1)
   result += tf.lgamma(tf.reduce_sum(alpha, -1))
   return result
@@ -42,7 +41,7 @@ rvs.Dirichlet.conjugate_log_prob = dirichlet_log_prob
 @_val_wrapper
 def bernoulli_log_prob(self, val):
   p = self.parameters['p']
-  f_val = tf.cast(val, np.float32)
+  f_val = tf.cast(val, tf.float32)
   return (f_val * tf.log(p) +
           (1. - f_val) * tf.log(1. - p))
 rvs.Bernoulli.conjugate_log_prob = bernoulli_log_prob
@@ -51,7 +50,7 @@ rvs.Bernoulli.conjugate_log_prob = bernoulli_log_prob
 @_val_wrapper
 def categorical_log_prob(self, val):
   p = self.parameters['p']
-  one_hot = tf.one_hot(val, p.get_shape()[-1], dtype=np.float32)
+  one_hot = tf.one_hot(val, p.get_shape()[-1], dtype=tf.float32)
   return tf.reduce_sum(tf.log(p) * one_hot, -1)
 rvs.Categorical.conjugate_log_prob = categorical_log_prob
 
@@ -60,7 +59,7 @@ rvs.Categorical.conjugate_log_prob = categorical_log_prob
 def gamma_log_prob(self, val):
   alpha = self.parameters['alpha']
   beta = self.parameters['beta']
-  result = (alpha - 1.) * tf.log(val)
+  result = (alpha - 1.0) * tf.log(val)
   result -= beta * val
   result += -tf.lgamma(alpha) + alpha * tf.log(beta)
   return result
@@ -70,7 +69,7 @@ rvs.Gamma.conjugate_log_prob = gamma_log_prob
 @_val_wrapper
 def poisson_log_prob(self, val):
   lam = self.parameters['lam']
-  f_val = tf.cast(val, np.float32)
+  f_val = tf.cast(val, tf.float32)
   result = f_val * tf.log(lam)
   result += -lam - tf.lgamma(f_val + 1)
   return result
@@ -84,7 +83,7 @@ def normal_log_prob(self, val):
   prec = tf.reciprocal(tf.square(sigma))
   result = prec * (-0.5 * tf.square(val) - 0.5 * tf.square(mu) +
                    val * mu)
-  result -= tf.log(sigma) + 0.5 * np.log(2 * np.pi)
+  result -= tf.log(sigma) + 0.5 * tf.log(2 * np.pi)
   return result
 rvs.Normal.conjugate_log_prob = normal_log_prob
 
