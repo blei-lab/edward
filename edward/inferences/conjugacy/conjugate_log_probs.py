5c2faa4fc73aab795e1c9f892dea075334d72c80
Dustin Tran <dustinviettran@gmail.com>
2017-04-17 10:09:51 -0400

    move conjugate_log_prob method writing to bottom

--- edward/inferences/conjugacy/conjugate_log_probs.py
+++ edward/inferences/conjugacy/conjugate_log_probs.py
@@ -25,7 +25,6 @@ def beta_log_prob(self, val):
   result += (b - 1.0) * tf.log(tf.constant(1.0) - val)
   result += -tf.lgamma(a) - tf.lgamma(b) + tf.lgamma(a + b)
   return result
-rvs.Beta.conjugate_log_prob = beta_log_prob
 
 
 @_val_wrapper
@@ -35,7 +34,6 @@ def dirichlet_log_prob(self, val):
   result += tf.reduce_sum(-tf.lgamma(alpha), -1)
   result += tf.lgamma(tf.reduce_sum(alpha, -1))
   return result
-rvs.Dirichlet.conjugate_log_prob = dirichlet_log_prob
 
 
 @_val_wrapper
@@ -44,7 +42,6 @@ def bernoulli_log_prob(self, val):
   f_val = tf.cast(val, tf.float32)
   return (f_val * tf.log(p) +
           (1. - f_val) * tf.log(1. - p))
-rvs.Bernoulli.conjugate_log_prob = bernoulli_log_prob
 
 
 @_val_wrapper
@@ -52,7 +49,6 @@ def categorical_log_prob(self, val):
   p = self.parameters['p']
   one_hot = tf.one_hot(val, p.get_shape()[-1], dtype=tf.float32)
   return tf.reduce_sum(tf.log(p) * one_hot, -1)
-rvs.Categorical.conjugate_log_prob = categorical_log_prob
 
 
 @_val_wrapper
@@ -63,7 +59,6 @@ def gamma_log_prob(self, val):
   result -= beta * val
   result += -tf.lgamma(alpha) + alpha * tf.log(beta)
   return result
-rvs.Gamma.conjugate_log_prob = gamma_log_prob
 
 
 @_val_wrapper
@@ -73,7 +68,6 @@ def poisson_log_prob(self, val):
   result = f_val * tf.log(lam)
   result += -lam - tf.lgamma(f_val + 1)
   return result
-rvs.Poisson.conjugate_log_prob = poisson_log_prob
 
 
 @_val_wrapper
@@ -85,7 +79,6 @@ def mvn_diag_log_prob(self, val):
                    val * mu)
   result -= tf.log(sigma) + 0.5 * tf.log(2 * np.pi)
   return result
-rvs.MultivariateNormalDiag.conjugate_log_prob = mvn_diag_log_prob
 
 
 @_val_wrapper
@@ -97,7 +90,6 @@ def normal_log_prob(self, val):
                    val * mu)
   result -= tf.log(sigma) + 0.5 * tf.log(2 * np.pi)
   return result
-rvs.Normal.conjugate_log_prob = normal_log_prob
 
 
 @_val_wrapper
@@ -108,4 +100,14 @@ def inverse_gamma_log_prob(self, val):
   result -= beta * tf.reciprocal(val)
   result += -tf.lgamma(alpha) + alpha * tf.log(beta)
   return result
+
+
+rvs.Beta.conjugate_log_prob = beta_log_prob
+rvs.Dirichlet.conjugate_log_prob = dirichlet_log_prob
+rvs.Bernoulli.conjugate_log_prob = bernoulli_log_prob
+rvs.Categorical.conjugate_log_prob = categorical_log_prob
+rvs.Gamma.conjugate_log_prob = gamma_log_prob
+rvs.Poisson.conjugate_log_prob = poisson_log_prob
+rvs.MultivariateNormalDiag.conjugate_log_prob = mvn_diag_log_prob
+rvs.Normal.conjugate_log_prob = normal_log_prob
 rvs.InverseGamma.conjugate_log_prob = inverse_gamma_log_prob
