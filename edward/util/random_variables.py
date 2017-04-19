9854a553560d02d9febc3b9a3a34381a562c0a6a
Dustin Tran <dustinviettran@gmail.com>
2017-04-18 21:44:48 -0400

    Speed up logic in ed.copy; distinguish reference tensor from tf.Variable (#616)
    * restyle test_copy.py
    
    * let copy(tf.Variable) output a tf.Variable
    
    * allow swap with tf.Variable; speed up via additional if's
    
    * avoid having to store a list of tf.Variable
    
    * clean up logic
    
    * move order of test functions in test_copy.py
    
    * remove already_present intermediate storage
    
    * handle copy_q=True and dict swap is tf.Tensor
    
    * avoid storing dict of random_variables

--- edward/util/random_variables.py
+++ edward/util/random_variables.py
@@ -7,7 +7,8 @@ import six
 import tensorflow as tf
 
 from copy import deepcopy
-from edward.models.random_variable import RandomVariable
+from edward.models.random_variable import \
+    RandomVariable, RANDOM_VARIABLE_COLLECTION
 from edward.util.graphs import random_variables
 from tensorflow.core.framework import attr_value_pb2
 from tensorflow.python.framework.ops import set_shapes_for_outputs
@@ -139,7 +140,7 @@ def copy(org_instance, dict_swap=None, scope="copied",
   >>> # `x` -> `z` <- y`, `qx`
   >>>
   >>> # This adds a subgraph with newly copied nodes,
-  >>> # `copied/qx` -> `copied/z` <- `copied/y`
+  >>> # `qx` -> `copied/z` <- `copied/y`
   >>> z_new = ed.copy(z, {x: qx})
   >>>
   >>> sess = tf.Session()
@@ -175,38 +176,40 @@ def copy(org_instance, dict_swap=None, scope="copied",
             return org_instance
           break
 
+  # If instance is a tf.Variable, return it; do not copy any. Note we
+  # check variables via their name. If we get variables through an
+  # op's inputs, it has type tf.Tensor and not tf.Variable.
+  if isinstance(org_instance, (tf.Tensor, tf.Variable)):
+    for variable in tf.global_variables():
+      if org_instance.name == variable.name:
+        if variable in dict_swap and replace_itself:
+          # Deal with case when `org_instance` is the associated _ref
+          # tensor for a tf.Variable.
+          org_instance = dict_swap[variable]
+          if not copy_q or isinstance(org_instance, tf.Variable):
+            return org_instance
+          for variable in tf.global_variables():
+            if org_instance.name == variable.name:
+              return variable
+          break
+        else:
+          return variable
+
   graph = tf.get_default_graph()
   new_name = scope + '/' + org_instance.name
 
-  # If an instance of the same name exists, return appropriately.
-  # Do this for ed.RandomVariable.
-  random_variables = {x.name: x for x in
-                      graph.get_collection('_random_variable_collection_')}
-  if new_name in random_variables:
-    return random_variables[new_name]
-
-  # Do this for tf.Tensor and tf.Operation.
-  try:
-    already_present = graph.as_graph_element(new_name,
-                                             allow_tensor=True,
-                                             allow_operation=True)
-    return already_present
-  except:
-    pass
-
-  # If instance is a tf.Variable, return it; do not re-copy any.
-  # Note we check variables via their name and not their type. This
-  # is because if we get variables through an op's inputs, it has
-  # type tf.Tensor: we can only tell it is a Variable via its name.
-  variables = {x.name: x for
-               x in graph.get_collection(tf.GraphKeys.GLOBAL_VARIABLES)}
-  if org_instance.name in variables:
-    return graph.get_tensor_by_name(variables[org_instance.name].name)
-
-  # Do the same for tf.placeholders.
-  if isinstance(org_instance, tf.Tensor) and \
-          "Placeholder" in org_instance.op.type:
-    return org_instance
+  # If an instance of the same name exists, return it.
+  if isinstance(org_instance, RandomVariable):
+    for rv in graph.get_collection(RANDOM_VARIABLE_COLLECTION):
+      if new_name == rv.name:
+        return rv
+  elif isinstance(org_instance, (tf.Tensor, tf.Operation)):
+    try:
+      return graph.as_graph_element(new_name,
+                                    allow_tensor=True,
+                                    allow_operation=True)
+    except:
+      pass
 
   if isinstance(org_instance, RandomVariable):
     rv = org_instance
@@ -230,6 +233,10 @@ def copy(org_instance, dict_swap=None, scope="copied",
   elif isinstance(org_instance, tf.Tensor):
     tensor = org_instance
 
+    # Do not copy tf.placeholders.
+    if 'Placeholder' in tensor.op.type:
+      return tensor
+
     # A tensor is one of the outputs of its underlying
     # op. Therefore copy the op itself.
     op = tensor.op
@@ -239,7 +246,7 @@ def copy(org_instance, dict_swap=None, scope="copied",
     new_tensor = new_op.outputs[output_index]
 
     # Add copied tensor to collections that the original one is in.
-    for name, collection in tensor.graph._collections.items():
+    for name, collection in six.iteritems(tensor.graph._collections):
       if tensor in collection:
         graph.add_to_collection(name, new_tensor)
 
