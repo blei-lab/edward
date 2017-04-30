13374d4048e99d18f366cfd8c45ce3a2dfb982c5
Dustin Tran <dustinviettran@gmail.com>
2017-04-30 01:19:11 -0400

    progress on diagnosing random issue with implicitklqp

---
+++ examples/temp.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python
+"""Bayesian linear regression. Inference uses data subsampling and
+scales the log-likelihood.
+
+One local optima is an inferred posterior mean of about [-5.0 5.0].
+This implies there is some weird symmetry happening; this result can
+be obtained by initializing the first coordinate to be negative.
+Similar occurs for the second coordinate.
+
+Note as with all GAN-style training, the algorithm is not stable. It
+is recommended to monitor training and halt manually according to some
+criterion (e.g., prediction accuracy on validation test, quality of
+samples).
+
+References
+----------
+http://edwardlib.org/tutorials/supervised-regression
+"""
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import edward as ed
+import numpy as np
+import tensorflow as tf
+
+from edward.models import Normal
+from tensorflow.contrib import slim
+
+
+def build_toy_dataset(N, w, noise_std=0.1):
+  D = len(w)
+  x = np.random.randn(N, D)
+  y = np.dot(x, w) + np.random.normal(0, noise_std, size=N)
+  return x, y
+
+
+def ratio_estimator(data, local_vars, global_vars):
+  # data[y] has shape (M,); global_vars[w] has shape (D,)
+  # we concatenate w to each data point y, so input has shape (M, 1 + D)
+  input = tf.concat([
+      tf.reshape(data[y], [M, 1]),
+      tf.tile(tf.reshape(global_vars[w], [1, D]), [M, 1])], 1)
+  # this is equivalent to previous. it has the error, meaning it is
+  # not slim. the initializations are the same, as iter 0 is same
+  # hidden = slim.fully_connected(input, 64, activation_fn=None,
+  #     weights_initializer=tf.random_normal_initializer(),
+  #     biases_initializer=None)
+  # output = slim.fully_connected(hidden, 1, activation_fn=None,
+  #     weights_initializer=tf.random_normal_initializer(),
+  #     biases_initializer=None)
+  w1 = tf.get_variable("w1", shape=[3, 64],
+      initializer=tf.random_normal_initializer())
+  w2 = tf.get_variable("w2", shape=[64, 1],
+      initializer=tf.random_normal_initializer())
+  hidden = tf.matmul(input, w1)
+  output = tf.matmul(hidden, w2)
+  return output
+
+
+ed.set_seed(42)
+
+M = 50  # batch size during training
+D = 2  # number of features
+
+# DATA
+w_true = np.ones(D) * 5.0
+X_batch, y_batch = build_toy_dataset(M, w_true)
+
+# MODEL
+X = X_batch.astype(np.float32)
+w = Normal(loc=tf.zeros(D), scale=tf.ones(D))
+y = Normal(loc=ed.dot(X, w), scale=tf.ones(M))
+
+# INFERENCE
+qw = Normal(loc=tf.Variable([1.0, 1.0]), scale=[1.0, 1.0])
+
+inference = ed.ImplicitKLqp(
+    {w: qw}, data={y: y_batch},
+    discriminator=ratio_estimator, global_vars={w: qw})
+inference.initialize(n_iter=50, n_print=10)
+
+sess = ed.get_session()
+tf.global_variables_initializer().run()
+
+# with tf.variable_scope("Disc", reuse=True):
+#   w2 = tf.get_variable("w2")
+#   print(w2.eval().reshape([64]))
+
+for _ in range(inference.n_iter):
+  info_dict = inference.update(variables="Disc")
+  t = info_dict['t']
+  if t == 1 or t % inference.n_print == 0:
+    print(info_dict['loss_d'])
