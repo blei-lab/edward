b3dce9c10d75d756410d0de9bf45a6d35bd8a0b1
Dustin Tran <dustinviettran@gmail.com>
2017-06-01 00:33:25 -0700

    update test_metrics.py

--- tests/test-criticisms/test_metrics.py
+++ tests/test-criticisms/test_metrics.py
@@ -9,16 +9,14 @@ from edward import criticisms
 
 all_classification_metrics = [
     criticisms.binary_accuracy,
-    criticisms.categorical_accuracy,
+    criticisms.sparse_categorical_accuracy,
+]
+
+all_real_classification_metrics = [
     criticisms.binary_crossentropy,
     criticisms.categorical_crossentropy,
     criticisms.hinge,
     criticisms.squared_hinge,
-]
-
-all_sparse_metrics = [
-    criticisms.sparse_categorical_accuracy,
-    criticisms.sparse_categorical_crossentropy,
 ]
 
 all_regression_metrics = [
@@ -30,32 +28,48 @@ all_regression_metrics = [
     criticisms.cosine_proximity,
 ]
 
+all_specialized_input_output_metrics = [
+    criticisms.categorical_accuracy,
+    criticisms.sparse_categorical_crossentropy,
+]
+
 
 class test_metrics_class(tf.test.TestCase):
 
   def test_classification_metrics(self):
     with self.test_session():
-      y_a = tf.convert_to_tensor(np.random.randint(0, 7, (6, 7)),
-                                 dtype=tf.float32)
-      y_b = tf.convert_to_tensor(np.random.random((6, 7)))
+      y_true = tf.convert_to_tensor(np.random.randint(0, 1, (2, 3)))
+      y_pred = tf.convert_to_tensor(np.random.randint(0, 1, (2, 3)))
       for metric in all_classification_metrics:
-        self.assertEqual(metric(y_a, y_b).eval().shape, ())
+        self.assertEqual(metric(y_true, y_pred).eval().shape, ())
 
-  def test_sparse_classification_metrics(self):
+  def test_real_classification_metrics(self):
     with self.test_session():
-      y_a = tf.convert_to_tensor(np.random.randint(0, 7, (6,)),
-                                 dtype=tf.float32)
-      y_b = tf.convert_to_tensor(np.random.random((6, 7)))
-      for metric in all_sparse_metrics:
-        self.assertEqual(metric(y_a, y_b).eval().shape, ())
+      y_true = tf.convert_to_tensor(np.random.randint(0, 5, (6, 7)))
+      y_pred = tf.random_normal([6, 7])
+      for metric in all_real_classification_metrics:
+        self.assertEqual(metric(y_true, y_pred).eval().shape, ())
 
   def test_regression_metrics(self):
     with self.test_session():
-      y_a = tf.convert_to_tensor(np.random.random((6, 7)))
-      y_b = tf.convert_to_tensor(np.random.random((6, 7)))
+      y_true = tf.random_normal([6, 7])
+      y_pred = tf.random_normal([6, 7])
       for metric in all_regression_metrics:
-        output = metric(y_a, y_b)
-        self.assertEqual(output.eval().shape, ())
+        self.assertEqual(metric(y_true, y_pred).eval().shape, ())
+
+  def test_specialized_input_output_metrics(self):
+    with self.test_session():
+      for metric in all_specialized_input_output_metrics:
+        if metric == criticisms.categorical_accuracy:
+          y_true = tf.convert_to_tensor(np.random.randint(0, 1, (6, 7)))
+          y_pred = tf.convert_to_tensor(np.random.randint(0, 7, (6,)))
+          self.assertEqual(metric(y_true, y_pred).eval().shape, ())
+        elif metric == criticisms.sparse_categorical_crossentropy:
+          y_true = tf.convert_to_tensor(np.random.randint(0, 5, (6, 7)))
+          y_pred = tf.random_normal([6, 7])
+          self.assertEqual(metric(y_true, y_pred).eval().shape, ())
+        else:
+          raise NotImplementedError()
 
 if __name__ == '__main__':
   tf.test.main()
