dc353f013b7862e7d16fa217008011a75f57fea1
Matt Hoffman <mhoffman@google.com>
2017-06-07 09:50:41 -0700

    Added a test

--- tests/test-inferences/test_metropolishastings.py
+++ tests/test-inferences/test_metropolishastings.py
@@ -18,20 +18,28 @@ class test_metropolishastings_class(tf.test.TestCase):
       mu = Normal(loc=0.0, scale=1.0)
       x = Normal(loc=tf.ones(50) * mu, scale=1.0)
 
-      qmu = Empirical(params=tf.Variable(tf.ones(2000)))
+      n_samples = 2000
+      qmu = Empirical(params=tf.Variable(tf.ones(n_samples)))
       proposal_mu = Normal(loc=0.0, scale=1.0)
 
       # analytic solution: N(loc=0.0, scale=\sqrt{1/51}=0.140)
       inference = ed.MetropolisHastings({mu: qmu},
                                         {mu: proposal_mu},
                                         data={x: x_data})
-      reset_ops = inference.reset()
       inference.run()
 
       self.assertAllClose(qmu.mean().eval(), 0, rtol=1e-2, atol=1e-2)
       self.assertAllClose(qmu.stddev().eval(), np.sqrt(1 / 51),
                           rtol=1e-2, atol=1e-2)
 
+      sess = ed.get_session()
+      old_t, old_n_accept = sess.run([inference.t, inference.n_accept])
+      self.assertEquals(old_t, n_samples)
+      self.assertGreater(old_n_accept, 0.1)
+      sess.run(inference.reset)
+      new_t, new_n_accept = sess.run([inference.t, inference.n_accept])
+      self.assertEquals(new_t, 0)
+      self.assertEquals(new_n_accept, 0)
 
 if __name__ == '__main__':
   ed.set_seed(42)
