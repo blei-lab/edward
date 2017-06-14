cd0fe9efb743ffe8ec09e9995793dcf66da27323
Dustin Tran <dustinviettran@gmail.com>
2017-06-13 21:15:05 -0700

    recommend keras<=2.0.4 for now as 2.0.5 breaks without explicit casting to tf.tensor

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
     extras_require={
         'tensorflow': ['tensorflow>=1.1.0rc0,<1.2.0rc0'],
         'tensorflow with gpu': ['tensorflow-gpu>=1.1.0rc0,<1.2.0rc0'],
-        'neural networks': ['keras>=1.0.0', 'prettytensor>=0.7.4'],
+        'neural networks': ['keras>=1.0.0,<=2.0.4', 'prettytensor>=0.7.4'],
         'notebooks': ['jupyter>=1.0.0'],
         'visualization': ['matplotlib>=1.3',
                           'pillow>=3.4.2',
