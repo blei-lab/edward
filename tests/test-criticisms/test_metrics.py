0a52d98f82db17b059d2130012b32abae73ca813
Dustin Tran <dustinviettran@gmail.com>
2017-06-18 22:13:25 -0700

    fix import error in tests by crudely importing *

--- tests/test-criticisms/test_metrics.py
+++ tests/test-criticisms/test_metrics.py
@@ -5,32 +5,32 @@ from __future__ import print_function
 import numpy as np
 import tensorflow as tf
 
-from edward import criticisms
+from edward.criticisms.evaluate import *
 
 all_classification_metrics = [
-    criticisms.binary_accuracy,
-    criticisms.sparse_categorical_accuracy,
+    binary_accuracy,
+    sparse_categorical_accuracy,
 ]
 
 all_real_classification_metrics = [
-    criticisms.binary_crossentropy,
-    criticisms.categorical_crossentropy,
-    criticisms.hinge,
-    criticisms.squared_hinge,
+    binary_crossentropy,
+    categorical_crossentropy,
+    hinge,
+    squared_hinge,
 ]
 
 all_regression_metrics = [
-    criticisms.mean_squared_error,
-    criticisms.mean_absolute_error,
-    criticisms.mean_absolute_percentage_error,
-    criticisms.mean_squared_logarithmic_error,
-    criticisms.poisson,
-    criticisms.cosine_proximity,
+    mean_squared_error,
+    mean_absolute_error,
+    mean_absolute_percentage_error,
+    mean_squared_logarithmic_error,
+    poisson,
+    cosine_proximity,
 ]
 
 all_specialized_input_output_metrics = [
-    criticisms.categorical_accuracy,
-    criticisms.sparse_categorical_crossentropy,
+    categorical_accuracy,
+    sparse_categorical_crossentropy,
 ]
 
 
@@ -60,11 +60,11 @@ class test_metrics_class(tf.test.TestCase):
   def test_specialized_input_output_metrics(self):
     with self.test_session():
       for metric in all_specialized_input_output_metrics:
-        if metric == criticisms.categorical_accuracy:
+        if metric == categorical_accuracy:
           y_true = tf.convert_to_tensor(np.random.randint(0, 1, (6, 7)))
           y_pred = tf.convert_to_tensor(np.random.randint(0, 7, (6,)))
           self.assertEqual(metric(y_true, y_pred).eval().shape, ())
-        elif metric == criticisms.sparse_categorical_crossentropy:
+        elif metric == sparse_categorical_crossentropy:
           y_true = tf.convert_to_tensor(np.random.randint(0, 5, (6)))
           y_pred = tf.random_normal([6, 7])
           self.assertEqual(metric(y_true, y_pred).eval().shape, ())
