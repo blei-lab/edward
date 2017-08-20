f94ef738b6cb9f34b1a1cb0702dac304e428607c
Dustin Tran <dustinviettran@gmail.com>
2017-08-19 17:19:29 -0700

    add test

--- tests/test-inferences/test_klqp.py
+++ tests/test-inferences/test_klqp.py
@@ -79,6 +79,10 @@ class test_klqp_class(tf.test.TestCase):
     self._test_normal_normal(ed.ScoreKLKLqp, n_samples=5, n_iter=5000)
     self._test_model_parameter(ed.ScoreKLKLqp, n_iter=50)
 
+  def test_score_rb_klqp(self):
+    self._test(ed.ScoreRBKLqp, n_samples=5, n_iter=5000)
+    self._test_model_parameter(ed.ScoreRBKLqp, n_iter=50)
+
 if __name__ == '__main__':
   ed.set_seed(42)
   tf.test.main()
