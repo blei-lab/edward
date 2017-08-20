cae9753a41945000b3c80f848ff7105be7f61520
Dustin Tran <dustinviettran@gmail.com>
2017-08-19 20:58:46 -0700

    update klqp test

--- tests/test-inferences/test_klqp.py
+++ tests/test-inferences/test_klqp.py
@@ -11,21 +11,20 @@ from edward.models import Normal
 
 class test_klqp_class(tf.test.TestCase):
 
-  def test_normalnormal_run(self):
+  def _test(self, Inference, *args, **kwargs):
     with self.test_session() as sess:
       x_data = np.array([0.0] * 50, dtype=np.float32)
 
       mu = Normal(loc=0.0, scale=1.0)
-      x = Normal(loc=tf.ones(50) * mu, scale=1.0)
+      x = Normal(loc=mu, scale=1.0, sample_shape=50)
 
       qmu_loc = tf.Variable(tf.random_normal([]))
       qmu_scale = tf.nn.softplus(tf.Variable(tf.random_normal([])))
       qmu = Normal(loc=qmu_loc, scale=qmu_scale)
 
       # analytic solution: N(loc=0.0, scale=\sqrt{1/51}=0.140)
-      n_iter = 5000
-      inference = ed.KLqp({mu: qmu}, data={x: x_data})
-      inference.run(n_iter=n_iter)
+      inference = Inference({mu: qmu}, data={x: x_data})
+      inference.run(*args, **kwargs)
 
       self.assertAllClose(qmu.mean().eval(), 0, rtol=1e-1, atol=1e-1)
       self.assertAllClose(qmu.stddev().eval(), np.sqrt(1 / 51),
@@ -34,11 +33,32 @@ class test_klqp_class(tf.test.TestCase):
       variables = tf.get_collection(
           tf.GraphKeys.GLOBAL_VARIABLES, scope='optimizer')
       old_t, old_variables = sess.run([inference.t, variables])
-      self.assertEqual(old_t, n_iter)
+      self.assertEqual(old_t, inference.n_iter)
       sess.run(inference.reset)
       new_t, new_variables = sess.run([inference.t, variables])
       self.assertEqual(new_t, 0)
       self.assertNotEqual(old_variables, new_variables)
+
+  def test_klqp(self):
+    self._test(ed.KLqp, n_iter=5000)
+
+  def test_reparameterization_entropy_klqp(self):
+    self._test(ed.ReparameterizationEntropyKLqp, n_iter=5000)
+
+  def test_reparameterization_klqp(self):
+    self._test(ed.ReparameterizationKLqp, n_iter=5000)
+
+  def test_reparameterization_kl_klqp(self):
+    self._test(ed.ReparameterizationKLKLqp, n_iter=5000)
+
+  def test_score_entropy_klqp(self):
+    self._test(ed.ScoreEntropyKLqp, n_samples=5, n_iter=5000)
+
+  def test_score_klqp(self):
+    self._test(ed.ScoreKLqp, n_samples=5, n_iter=5000)
+
+  def test_score_kl_klqp(self):
+    self._test(ed.ScoreKLKLqp, n_samples=5, n_iter=5000)
 
 if __name__ == '__main__':
   ed.set_seed(42)
