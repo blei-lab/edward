51822669025bc08753ceda56a0adce3ffd436cb7
cshenton <charlieshenton@me.com>
2017-09-21 09:01:32 +1000

    tests check support along each dimension for transform

--- tests/test-util/test_transform.py
+++ tests/test-util/test_transform.py
@@ -14,6 +14,12 @@ from tensorflow.contrib.distributions import bijectors
 
 class test_transform_class(tf.test.TestCase):
 
+  def assertSamplePosNeg(self, sample):
+    num_pos = np.sum((sample > 0.0), axis=0, keepdims=True)
+    num_neg = np.sum((sample < 0.0), axis=0, keepdims=True)
+    self.assertTrue((num_pos > 0).all())
+    self.assertTrue((num_neg > 0).all())
+
   def test_args(self):
     with self.test_session():
       x = Normal(-100.0, 1.0)
@@ -34,8 +40,7 @@ class test_transform_class(tf.test.TestCase):
       y = ed.transform(x)
       self.assertIsInstance(y, TransformedDistribution)
       sample = y.sample(10, seed=1).eval()
-      self.assertTrue((sample > 0.0).any())
-      self.assertTrue((sample < 0.0).any())
+      self.assertSamplePosNeg(sample)
 
   def test_nonnegative(self):
     with self.test_session():
@@ -43,18 +48,17 @@ class test_transform_class(tf.test.TestCase):
       y = ed.transform(x)
       self.assertIsInstance(y, TransformedDistribution)
       sample = y.sample(10, seed=1).eval()
-      self.assertTrue((sample > 0.0).any())
-      self.assertTrue((sample < 0.0).any())
+      self.assertSamplePosNeg(sample)
 
   def test_simplex(self):
     with self.test_session():
-      x = Dirichlet([1.0, 2.0, 3.0, 4.0])
+      x = Dirichlet([1.1, 1.2, 1.3, 1.4])
       y = ed.transform(x)
       self.assertIsInstance(y, TransformedDistribution)
       sample = y.sample(10, seed=1).eval()
+      print(sample.shape)
       print(sample)
-      self.assertTrue((sample > np.zeros(3)).any())
-      self.assertTrue((sample < np.zeros(3)).any())
+      self.assertSamplePosNeg(sample)
 
   def test_real(self):
     with self.test_session():
@@ -62,16 +66,14 @@ class test_transform_class(tf.test.TestCase):
       y = ed.transform(x)
       self.assertIsInstance(y, Normal)
       sample = y.sample(10, seed=1).eval()
-      self.assertTrue((sample > 0.0).any())
-      self.assertTrue((sample < 0.0).any())
+      self.assertSamplePosNeg(sample)
 
   def test_multivariate_real(self):
     with self.test_session():
       x = MultivariateNormalDiag(tf.zeros(2), tf.ones(2))
       y = ed.transform(x)
       sample = y.sample(10, seed=1).eval()
-      self.assertTrue((sample > np.zeros(2)).any())
-      self.assertTrue((sample < np.zeros(2)).any())
+      self.assertSamplePosNeg(sample)
 
   def test_no_support(self):
     with self.test_session():
